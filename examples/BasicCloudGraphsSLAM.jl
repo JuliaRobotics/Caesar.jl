fcef6ba61649e162dac277a2fc9679f0da6c0d70
dehann <dehann.fourie@gmail.com>
2016-07-05 19:22:01 -0400

    its working...

--- examples/BasicCloudGraphsSLAM.jl
+++ examples/BasicCloudGraphsSLAM.jl
@@ -18,6 +18,23 @@ fg = emptyFactorGraph() # this will is being replaced by cloudGraph, added here 
 
 # robot style, add first pose vertex
 v1 = addNode!(fg,"x1",doors,N=N)
-
 cv1 = CloudGraphs.exVertex2CloudVertex(v1);
 CloudGraphs.add_vertex!(cloudGraph, cv1);
+
+# add second pose vertex
+tem = 2.0*randn(1,N)+getVal(v1)+50.0
+v2 = addNode!(fg, "x2", tem, N=N)
+cv2 = CloudGraphs.exVertex2CloudVertex(v2);
+CloudGraphs.add_vertex!(cloudGraph, cv2);
+
+# now add the odometry factor between them
+# but first we need to register the new types we will be using
+CloudGraphs.registerPackedType!(cloudGraph, FunctionNodeData{Odo}, FunctionNodeData{PackedOdo}, encodingConverter=FNDencode, decodingConverter=FNDencode)
+
+f1 = addFactor!(fg,[v1;v2],Odo([50.0]',[2.0]',[1.0]))
+cvA = CloudGraphs.exVertex2CloudVertex(f1);
+CloudGraphs.add_vertex!(cloudGraph, cvA);
+ceA1 = CloudGraphs.CloudEdge(cv1, cvA, "DEPENDENCE");
+ceA2 = CloudGraphs.CloudEdge(cv2, cvA, "DEPENDENCE");
+CloudGraphs.add_edge!(cloudGraph, ceA1);
+CloudGraphs.add_edge!(cloudGraph, ceA2);
