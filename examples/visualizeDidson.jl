1294ae50401e6a0810b1d8b2f077610716bbd126
dehann <dehann.fourie@gmail.com>
2016-12-21 13:33:24 -0500

    update example

--- examples/visualizeDidson.jl
+++ examples/visualizeDidson.jl
@@ -8,46 +8,14 @@ using Caesar
 # using Base.Test
 
 
-# using CoordinateTransformations, GeometryTypes, DrakeVisualizer, ColorTypes
 
-
-#
-# function visualizeDensityMesh!(vc::VisualizationContainer, fgl::FactorGraph, lbl::Symbol; levels=3)
-#
-#   pl1 = marginal(getVertKDE(fgl,lbl),[1;2;3])
-#
-#   gg = (x, a=0.0) -> evaluateDualTree(pl1, ([x[1];x[2];x[3]]')')[1]-a
-#
-#   x = getKDEMax(pl1)
-#   maxval = gg(x)
-#
-#   vv = getKDERange(pl1)
-#   lower_bound = Vec(vec(vv[:,1])...)
-#   upper_bound = Vec(vec(vv[:,2])...)
-#
-#   levels = linspace(0.0,maxval,levels+2)
-#
-#   MD = []
-#   for val in levels[2:(end-1)]
-#     meshdata = GeometryData(contour_mesh(x -> gg(x,val), lower_bound, upper_bound))
-#     meshdata.color = RGBA( val/(1.5*maxval),0.0,1.0,val/(1.5*maxval))
-#     push!(MD, meshdata)
-#   end
-#   mptr = Link(MD)
-#   vc.meshes[lbl] = mptr
-#   Visualizer(mptr, 2) # meshdata
-#   nothing
-# end
-#
-
+# start visualizer
+vc = startdefaultvisualization()
 
 
 # okay build the graph
 fg = initfg()
-N = 300
-
-# start visualizer
-vc = startdefaultvisualization()
+N = 200
 
 
 initCov = 0.01*eye(6)
@@ -74,16 +42,6 @@ addFactor!(fg,[v0;v1],meas)
 visualizeallposes!(vc, fg)
 
 
-
-
-# solve
-tree = wipeBuildNewTree!(fg)
-[inferOverTree!(fg, tree) for i in 1:1];
-
-
-visualizeDensityMesh!(vc, fg, :l1)
-
-
 #
 visualizeDensityMesh!(vc, fg, :l1)
 
@@ -91,7 +49,7 @@ visualizeDensityMesh!(vc, fg, :l1)
 
 
 
-odo = SE3([3.0;3.0;0.0], Euler(0.0,0.0, -pi/2.0) )
+odo = SE3([2.7;2.7;0.0], Euler(0.0,0.0, -pi/2.0) )
 # v, f = addPose3Pose3(fg, "x3", deepcopy(odo), odoCov, N=N)
 addOdoFG!(fg, Pose3Pose3(deepcopy(odo), odoCov) )
 
@@ -102,6 +60,17 @@ addFactor!(fg,[getVert(fg, :x2);v1],meas2)
 
 
 
+
+
+
+# solve
+tree = wipeBuildNewTree!(fg)
+[inferOverTree!(fg, tree) for i in 1:1];
+
+
+
+visualizeallposes!(vc, fg)
+visualizeDensityMesh!(vc, fg, :l1)
 
 
 
