a55086f7b546eb8848263d71acc9763e40ce509c
dehann <dehann.fourie@gmail.com>
2017-01-09 15:00:20 -0500

    temp data

--- test/diffErrAnalysis.jl
+++ test/diffErrAnalysis.jl
@@ -3,35 +3,6 @@
 using Caesar, RoME, IncrementalInference, KernelDensityEstimate, Gadfly, Colors, Cairo
 using HDF5, JLD
 
-function rangeErrMaxPoint2(fgl1::FactorGraph, id1, fgl2::FactorGraph ,id2)
-  mv1 = getKDEMax(getVertKDE(fgl1,id1))
-  mv2 = getKDEMax(getVertKDE(fgl2,id2))
-  return norm(mv1[1:2]-mv2[1:2])
-end
-
-function rangeCompAllPoses(fgl1::FactorGraph, fgl2::FactorGraph)
-  ranges = Float64[]
-  xx,ll = ls(fgl1)
-  for x in xx
-    push!(ranges, rangeErrMaxPoint2(fgl1,x,fgl2,x))
-  end
-  return ranges
-end
-
-function rangeCompAllPoses(
-    valsbaseline::Dict{Int64,Array{Float64,1}},
-    fglbaseline::FactorGraph,
-    fgltest::FactorGraph)
-
-  ranges = Float64[]
-  xx,ll = ls(fgltest)
-  for x in xx
-    mv1 = valsbaseline[fglbaseline.IDs[x]]
-    mv2 = getKDEMax(getVertKDE(fgltest,x))
-    push!(ranges, norm(mv1[1:2]-mv2[1:2]))
-  end
-  return ranges
-end
 
 @load "results/fgT1400_Nightly7_28_perErr0.0.jld"
 MMMAP0 = deepcopy(fg2)
