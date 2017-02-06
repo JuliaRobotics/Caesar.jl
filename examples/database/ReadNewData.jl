ea5d4542187e48cf26f1e6b30a63b5390cbeb878
dehann <dehann.fourie@gmail.com>
2017-02-06 16:19:59 -0500

    wip on reading data from neo4j

---
+++ examples/database/ReadNewData.jl
@@ -0,0 +1,85 @@
+# find and interact with NEWDATA in neo4j
+
+
+using Caesar, RoME
+using CloudGraphs, Neo4j
+
+using JSON
+
+# # # connect to the server, CloudGraph stuff
+# dbaddress = length(ARGS) > 0 ? ARGS[1] : "localhost"
+# println("Taking Neo4j database address as $(dbaddress)...")
+#
+# dbusr = length(ARGS) > 1 ? ARGS[2] : ""
+# dbpwd = length(ARGS) > 2 ? ARGS[3] : ""
+#
+# mongoaddress = length(ARGS) > 3 ? ARGS[4] : "localhost"
+# println("Taking Mongo database address as $(mongoaddress)...")
+#
+# session = length(ARGS) > 4 ? string(ARGS[5]) : ""
+# println("Attempting to draw session $(session)...")
+#
+# DRAWDEPTH = length(ARGS) > 5 ? ARGS[6]=="drawdepth" : false
+
+# TODO comment out for command line operation
+include(joinpath(dirname(@__FILE__),"blandauthremote.jl"))
+session = "SESSROX"
+
+
+# Connection to database
+configuration = CloudGraphs.CloudGraphConfiguration(dbaddress, 7474, dbusr, dbpwd, mongoaddress, 27017, false, "", "");
+cloudGraph = connect(configuration);
+conn = cloudGraph.neo4j.connection
+# register types of interest in CloudGraphs
+registerGeneralVariableTypes!(cloudGraph)
+Caesar.usecloudgraphsdatalayer!()
+
+
+loadtx = transaction(conn)
+query = "match (n:$(session))-[r1]-(f:NEWDATA:$(session):FACTOR)-[r2]-(m:NEWDATA:$(session)) return distinct n, f, m"
+cph = loadtx(query, submit=true)
+# loadresult = commit(loadtx)
+
+# @show cph.results[1]
+
+newposedict = Dict{Symbol, Int}()
+
+for val in cph.results[1]["data"]
+  i = 0
+  for elem in val["meta"]
+    # @show elem["type"]
+    i+=1
+    rdict =  JSON.parse(val["row"][i]["frontend"])
+    @show rdict["type"]
+    if uppercase(rdict["type"])=="POSE"
+      npsym = Symbol(string("x",parse(Int, rdict["userid"])+1)) # TODO -- fix :x0 requirement
+      newposedict[npsym] = elem["id"]
+    end
+  end
+  println()
+end
+
+@show newposedict
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#
--- examples/database/python/neo_interaction.py
+++ examples/database/python/neo_interaction.py
@@ -2,8 +2,9 @@ from neo4j.v1 import GraphDatabase, basic_auth # via queries
 import json
 import random#.uniform as ru
 
-authfile = '/home/rmata/neo_authfile.txt' # username on one line, password on next (for database)
-un,pw = open(authfile).read().splitlines()
+# authfile = '/home/rmata/neo_authfile.txt' # username on one line, password on next (for database)
+# un,pw = open(authfile).read().splitlines()
+un,pw = "neo4j","profroot"
 
 #####################################################################################
 ## Database Construction in Python Example
@@ -27,28 +28,31 @@ for i in range(NUM_ODOM_NODES):
 
     # generate odometry constraint between old_odom_num and curr_odom_num
     add_landmark = random.randint(0,1) # whether or not to add landmark to a certain odom
-    loc = random.uniform(old_odom_num, curr_odom_num) # distance between them: goes into factor
+    # loc = random.uniform(old_odom_num, curr_odom_num) # distance between them: goes into factor
+    loc = "50 0 0.523599 100 0 0 100 0 500"
 
     # encode JSON string to add to slamstring property of new4j node
-    var_b4jso1 = {'odom_num':"ODOM " + str(old_odom_num)}
-    var_b4jso2 = {'odom_num':"ODOM " + str(curr_odom_num)}
+    var_b4jso1 = {'type':'POSE', 'userid': str(old_odom_num)}
+    var_b4jso2 = {'type':'POSE', 'userid': str(curr_odom_num)}
     var_json_str1 = json.dumps(var_b4jso1)
     var_json_str2 = json.dumps(var_b4jso2)
 
-    fac_b4jso = {'slam_info':"ODOM " + str(old_odom_num) + " " + str(curr_odom_num) + " " + str(loc)}
+    odo_str = "ODOM " + str(old_odom_num) + " " + str(curr_odom_num) + " " + str(loc)
+    fac_b4jso = {'slamstring':odo_str, 'type': 'FACTOR'}
     fac_json_str = json.dumps(fac_b4jso)
+    # fac_json_str = odo_str
 
     if old_odom_num == 0:
-        session.run("MERGE (o1:POSE:SESSROX:NEWDATA {slam_info: {var_info1} }) " # finds, creates if not exist
-                    "MERGE (f:FACTOR:SESSROX:NEWDATA { slam_info: {fac_info} }) " # prior factor
+        session.run("MERGE (o1:POSE:SESSROX:NEWDATA { frontend: {var_info1} }) " # finds, creates if not exist
+                    "MERGE (f:FACTOR:SESSROX:NEWDATA { frontend: {fac_info} }) " # prior factor
                     "MERGE (o1)-[:REL]-(f)",
                     {"var_info1": var_json_str1,
-                     "fac_info":json.dumps({"prior_dist":"prior distribution etc"})})
+                     "fac_info":json.dumps({'slamstring':'GAUSS 0 0 0 0 100 0 0 100 0 500', 'type': 'FACTOR'})})
 
     # create nodes (if not already): property/key "slamstring", value JSON string?
-    session.run("MERGE (o1:POSE:SESSROX:NEWDATA {slam_info: {var_info1} }) " # finds, creates if not exist
-                "MERGE (o2:POSE:SESSROX:NEWDATA { slam_info: {var_info2} })"
-                "MERGE (f:FACTOR:SESSROX:NEWDATA { slam_info: {fac_info} }) " # odom-odom factor
+    session.run("MERGE (o1:POSE:SESSROX:NEWDATA { frontend: {var_info1} }) " # finds, creates if not exist
+                "MERGE (o2:POSE:SESSROX:NEWDATA { frontend: {var_info2} })"
+                "MERGE (f:FACTOR:SESSROX:NEWDATA { frontend: {fac_info} }) " # odom-odom factor
                 "MERGE (o1)-[:REL]->(f) " # add relationships
                 "MERGE (o2)-[:REL]->(f) ",
                 {"var_info1":var_json_str1,
@@ -61,15 +65,16 @@ for i in range(NUM_ODOM_NODES):
                                                max(old_odom_num, landmark_num))
         # measure between them: goes into factor
 
-        fac_b4jso_land = {'land_info':"ODOM " + str(old_odom_num) +
-                          " LAND " + str(landmark_num) + " " +
-                          str(landmark_old_odom_loc)} # whatever the odom-factor should contain
+        fac_b4jso_land = {'slamstring':"LANDMARK " + str(old_odom_num) +
+                          " " + str(landmark_num) + " " +
+                          str(landmark_old_odom_loc),
+                          'type': 'FACTOR'} # whatever the odom-factor should contain
         land_fac_json_str = json.dumps(fac_b4jso_land)
-        land_var_json = json.dumps({"tag_id":landmark_num})
+        land_var_json = json.dumps({'tag_id':landmark_num, 'type':'LANDMARK'})
         # add observation/new location var to current odom (IE new factor node, new POSE)
-        session.run("MERGE (l1:LANDMARK:SESSROX:NEWDATA {slam_info: {land_info}})"
-                    "MERGE (o1:POSE:SESSROX:NEWDATA {slam_info: {var_info}})"
-                    "MERGE (f:FACTOR:SESSROX:NEWDATA {slam_info: {fac_info}})" # odom-landmark factor
+        session.run("MERGE (l1:LANDMARK:SESSROX:NEWDATA {frontend: {land_info}})"
+                    "MERGE (o1:POSE:SESSROX:NEWDATA {frontend: {var_info}})"
+                    "MERGE (f:FACTOR:SESSROX:NEWDATA {frontend: {fac_info}})" # odom-landmark factor
                     "MERGE (o1)-[:REL]->(f) "
                     "MERGE (f)-[:REL]->(l1) ",
                     {"land_info": land_var_json,
