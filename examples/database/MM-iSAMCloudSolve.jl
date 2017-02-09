80eb0553a7f800477d30c2b43da160e7d9113f56
dehann <dehann.fourie@gmail.com>
2017-02-09 13:33:29 -0500

    updates to db solving

--- examples/database/MM-iSAMCloudSolve.jl
+++ examples/database/MM-iSAMCloudSolve.jl
@@ -1,6 +1,8 @@
 using Caesar, RoME
 using CloudGraphs, Neo4j
 
+# using IncrementalInference
+
 # connect to the server, CloudGraph stuff
 dbaddress = length(ARGS) > 0 ? ARGS[1] : "localhost"
 dbusr = length(ARGS) > 1 ? ARGS[2] : ""
@@ -11,6 +13,10 @@ mongoaddress = length(ARGS) > 3 ? ARGS[4] : "localhost"
 session = length(ARGS) > 4 ? string(ARGS[5]) : ""
 
 println("Attempting to solve session $(session)...")
+
+
+# include(joinpath(dirname(@__FILE__),"blandauthremote.jl"))
+# session = "SESSROX"
 
 
 # Connect to database
@@ -30,11 +36,12 @@ while true
   setBackendWorkingSet!(conn, session)
 
   println("get local copy of graph")
+
   # removeGenericMarginals!(conn) # function should not be necessary, but fixes a minor bug following elimination algorithm
   if fullLocalGraphCopy!(fg, conn)
     tree = wipeBuildNewTree!(fg,drawpdf=false)
     # removeGenericMarginals!(conn)
-
+    
     # while true # repeat while graph unchanged
       # okay now do the solve
       inferOverTree!(fg, tree, N=100)
