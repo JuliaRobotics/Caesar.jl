edd5243615a5e71b9ca2829625c9a1fdcdc7629e
dehann <dehann.fourie@gmail.com>
2017-02-12 16:42:29 -0500

    rework newdata script

--- examples/database/ReadNewData.jl
+++ examples/database/ReadNewData.jl
@@ -40,144 +40,15 @@ Caesar.usecloudgraphsdatalayer!()
 
 N=100
 fg = Caesar.initfg(sessionname=session, cloudgraph=cloudGraph)
+
+# setBackendWorkingSet!(conn, session)
+fullLocalGraphCopy!(fg, conn)
+
+@show length(fg.IDs)
+
 updatenewverts!(fg, N=N)
 
-@show
-
-# loadtx = transaction(conn)
-# query = "match (n:$(session))-[:DEPENDENCE]-(f:NEWDATA:$(session):FACTOR) return distinct n, f"
-# cph = loadtx(query, submit=true)
-# # loadresult = commit(loadtx)
-#
-# # @show cph.results[1]
-#
-# newvertdict = Dict{Int, Dict{Symbol,Dict{AbstractString,Any}}}()
-# # mongokeydict = Dict{Int, Dict{AbstractString,Any}}()
-#
-#
-# for val in cph.results[1]["data"]
-#   i = 0
-#   for elem in val["meta"]
-#     # @show elem["type"]    # @show rdict["type"]
-#     i+=1
-#     rdict = JSON.parse(val["row"][i]["frtend"])
-#     newvertdict[elem["id"]] = Dict{Symbol, Dict{AbstractString,Any}}()
-#     newvertdict[elem["id"]][:frtend] = rdict
-#     if haskey(val["row"][i], "mongo_keys")
-#       # @show val["row"][i]["mongo_keys"]
-#       newvertdict[elem["id"]][:mongokeys] = JSON.parse(val["row"][i]["mongo_keys"])
-#     end
-#     # if uppercase(rdict["type"])=="POSE" || uppercase(rdict["type"])=="FACTOR"
-#       # npsym = Symbol(string("x",parse(Int, rdict["userid"])+1)) # TODO -- fix :x0 requirement
-#   end
-#   # println()
-# end
-#
-# # @show newvertdict
-#
-# # for elem in newvertdict
-# #   @show elem[2]["t"]
-# #   @show collect(keys(elem[2]))
-# #   if elem[2]["t"] == "P"
-# #     @show elem[1], elem[2]["uid"]
-# #   elseif elem[2]["t"] == "L"
-# #     @show elem[1], elem[2]["uid"], elem[2]["tag_id"]
-# #   end
-# #   println()
-# # end
-#
-#
-# # next step is to convert theis data into actual usable graph for IIF.
-# # We'll do this with CloudGraphs.updatevertex!(...)
-#
-# # first iteration, lets use addNode/addFactor to local graph only and
-# # then call updateFullCloudVertData!. ID tracking may be the issue here.
-#
-#
-# for (neoNodeId,elem) in newvertdict
-#   # neoNodeId = 31369
-#   @show neoNodeId
-#   if elem[:frtend]["t"] == "P"
-#     uidl = elem[:frtend]["uid"]+1
-#     nlbsym = Symbol(string('x', uidl))
-#     v = addNode!(fg, nlbsym, 0.1*randn(3,N), 0.01*eye(3), N=N, ready=0, uid=uidl,api=localapi)
-#     # v.attributes["mongo_key_hack"] = elem[]
-#     insertValuesCloudVert!(fg, neoNodeId, elem, uidl, v, labels=["POSE";"$(session)"])
-#   elseif elem[:frtend]["t"] == "L"
-#     uidl = elem[:frtend]["tag_id"]+200000 # TODO complete hack
-#     nlbsym = Symbol(string('l', uidl))
-#     v = addNode!(fg, nlbsym, 0.1*randn(2,N), 0.01*eye(3), N=N, ready=0, uid=uidl,api=localapi)
-#     insertValuesCloudVert!(fg, neoNodeId, elem, uidl, v, labels=["LANDMARK";"$(session)"])
-#   end
-# end
-#
-#
-# warn("using hack counter for FACTOR uid")
-# fuid = 100000
-# # neoNodeId = 63363
-# # elem = newvertdict[neoNodeId]
-# for (neoNodeId,elem) in newvertdict
-#   if elem[:frtend]["t"] == "F"
-#     @show neoNodeId
-#     # verts relating to this factor
-#     verts = Vector{Graphs.ExVertex}()
-#     i=0
-#     for bf in split(elem[:frtend]["btwn"], ' ')
-#       i+=1
-#       uid = 0
-#       if elem[:frtend]["lklh"][1] == 'P' || i==1
-#         uid = parse(Int,bf)+1
-#       else
-#         uid = parse(Int,bf)+200000
-#       end
-#       push!(verts, fg.g.vertices[uid])
-#     end
-#     # the factor type
-#     usrfnc = recoverConstraintType(elem[:frtend])
-#     fuid += 1
-#     vert = addFactor!(fg, verts, usrfnc, ready=0, api=localapi, uid=fuid)
-#     insertValuesCloudVert!(fg, neoNodeId, elem, fuid, vert, labels=["FACTOR";"$(session)"])
-#   end
-# end
-#
-#
-#
-# ls(fg)
-
-
-# This one is breaking
-# neoNodeId = 63433
-# elem = newvertdict[neoNodeId]
-#
-# verts = Vector{Graphs.ExVertex}()
-# for bf in split(elem["btwn"], ' ')
-#   uid = parse(Int,bf)+1
-#   push!(verts, fg.g.vertices[uid])
-# end
-# # the factor type
-# @show usrfnc = recoverConstraintType(elem)
-#
-# fuid += 1
-# vert = addFactor!(fg, verts, usrfnc, ready=0, api=localapi, uid=fuid)
-#
-# fg.cgIDs[fuid] = neoNodeId
-# cv = exVertex2CloudVertex( vert )
-# @show cv.neo4jNodeId = neoNodeId
-# cv.neo4jNode = Neo4j.getnode(fg.cg.neo4j.graph, neoNodeId)
-# cv.isValidNeoNodeId = true
-# cv.labels=["FACTOR";"$(session)"]
-#
-# @show typeof(cv.packed.fnc.usrfnc!)
-#
-# CloudGraphs.update_vertex!(fg.cg, cv)
-
-# function addFactor!{I <: Union{FunctorInferenceType, InferenceType}, T <: AbstractString}(fgl::FactorGraph,
-#       Xi::Array{Graphs.ExVertex,1},
-#       usrfnc::I;
-#       ready::Int=1,
-#       api::DataLayerAPI=dlapi,
-#       labels::Vector{T}=String[] )
-
+@show length(fg.IDs)
 
 
 
