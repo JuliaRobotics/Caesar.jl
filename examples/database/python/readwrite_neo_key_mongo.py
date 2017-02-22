12aebf16961ff6a08d1aa900e9df48eff19a49ff
Sudeep Pillai <spillai@csail.mit.edu>
2017-02-22 11:53:57 -0500

    updated readwrite neo mongo

--- examples/database/python/readwrite_neo_key_mongo.py
+++ examples/database/python/readwrite_neo_key_mongo.py
@@ -1,5 +1,7 @@
 # Fetch MongoKeys per key frame and re-insert
-
+import cv2
+import numpy as np
+import os
 from neo4j.v1 import GraphDatabase, basic_auth # via queries
 from pymongo import MongoClient
 import pymongo
@@ -9,7 +11,7 @@ import json # for
 
 
 
-authfileneo = '/home/dehann/neo_authfile.txt' # username on one line, password on next (for database)
+authfileneo = os.path.join(os.getenv('HOME'), 'neo_authfile.txt') # username on one line, password on next (for database)
 un,pw,addrn = open(authfileneo).read().splitlines()
 
 driver = GraphDatabase.driver(addrn, auth=basic_auth(un, pw))
@@ -31,7 +33,7 @@ for rec in recs:
 # Add a binary image entry (referred to as a "document") to a "collection" in a mongo database.
 #####################################################################################
 
-authfilemongo = '/home/dehann/mongo_authfile.txt' # username on one line, password on next (for database)
+authfilemongo = os.path.join(os.getenv('HOME'), 'mongo_authfile.txt') # username on one line, password on next (for database)
 addrm = open(authfilemongo).read().splitlines()
 client = MongoClient(addrm)
 
@@ -42,14 +44,19 @@ print ""
 #USING NEW DATA
 # connect to database using client
 db = client.CloudGraphs # tester==name of the db. also db = client['tester'] works.
+# db = client.tester # tester==name of the db. also db = client['tester'] works.
+
 collection = "bindata"
+# collection = "rgb_keyframes"
 
-#New PNG dataset
+
+# New PNG dataset
 key = ObjectId("58ab6b015d76250947c22498")
+# key = ObjectId("589b7535093d3f1d51cc81da")
 
+results = db[collection].find({"_id":key})
 
 # get the image?
-for result in db[collection].find({"_id":key}):
-    newf = open('/home/gearsad/newfile.png', 'w') # to write image to
-    newf.write(result["val"]);
-    newf.close()
+for item in results:
+  im = cv2.imdecode(np.fromstring(str(item['val']), dtype=np.uint8), -1)
+  print im.shape
