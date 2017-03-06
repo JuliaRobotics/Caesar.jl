6df2c7f0cba2cc211f3e6ddfbc1715c8c8b0663c
Sudeep Pillai <spillai@csail.mit.edu>
2017-03-06 08:36:02 -0500

    final updates to segnet extract - camera ready

--- examples/database/python/segnet_extract.py
+++ examples/database/python/segnet_extract.py
@@ -11,6 +11,7 @@ import sys
 import argparse
 import cv2
 import numpy as np
+import time
 
 from neo4j.v1 import GraphDatabase, basic_auth
 from pymongo import MongoClient
@@ -20,9 +21,8 @@ import json
 
 from pybot.utils.io_utils import create_path_if_not_exists
 from pybot.vision.image_utils import im_resize, to_color, to_gray
-sys.path.append(os.path.join(os.getenv('PYCAFFE'), 'python'))
 
-from pybot.vision.caffe import setup_segnet
+sys.path.append(os.path.join(os.getenv('PYCAFFE'), 'python'))
 
 class SegnetExtractor(object):
     def __init__(self, directory):
@@ -58,7 +58,6 @@ def updatemongokeys(session, dbsessname, neoid, oidname, newoid):
     if mymoids == None:
         mymoids = {}
     else:
-        print 'trying to load json'
         mymoids = json.loads(mymoids)
 
     mymoids[oidname] = newoid
@@ -90,7 +89,19 @@ if __name__ == "__main__":
     parser.add_argument(
         '-d', '--directory', type=str, default='', required=True, 
         help='Segnet directory')
+    parser.add_argument(
+        '-g', '--gpu', type=int, default=0, required=True, 
+        help='GPU ID')
+    parser.add_argument(
+        '-S', '--session', type=str, default='', required=True, 
+        help='Session name')
     args = parser.parse_args()
+    print('Using session {}'.format(args.session))
+    print('Using GPU {}'.format(args.gpu))
+
+    import caffe
+    caffe.set_device(args.gpu)
+    from pybot.vision.caffe import setup_segnet
     
     # username on one line, password on next (for database)
     authfileneo = os.path.join(os.getenv('HOME'), 'neo_authfile.txt') 
@@ -110,33 +121,28 @@ if __name__ == "__main__":
     # connect to database using client
     # db = client.tester # tester==name of the db. also db = client['tester'] works.
 
-    # update and repeat this
-    last_neo_id = 0
-    recs = session.run('''match (n:SESSTURTLE:POSE) where id(n) > {} '''
-                       '''return id(n) as id, n.mongo_keys as mongokeys, '''
-                       '''n.frtend as label order by id(n) asc limit 1'''.format(last_neo_id))
-
     # Setup segnet
     segnet = SegnetExtractor(os.path.expanduser(args.directory))
     
     # iterate through the poses and get the mongokeys
-    for index in range(0,9): 
+    last_neo_id = 0
+    for index in range(198):
+        recs = session.run('''match (n:{}:POSE) where id(n) > {} '''
+                           '''return id(n) as id, n.mongo_keys as mongokeys, '''
+                           '''n.frtend as label order by id(n) asc limit 1'''.format(args.session, last_neo_id))
         for rec in recs:
-          print 'record', rec["id"], rec["label"], rec["mongokeys"]
+          # print 'record', rec["id"], rec["label"], rec["mongokeys"]
           last_neo_id = rec["id"]
-          recs = session.run('''match (n:SESSTURTLE:POSE) where id(n) > {} '''
-                             '''return id(n) as id, n.mongo_keys as mongokeys, '''
-                             '''n.frtend as label order by id(n) asc limit 1'''.format(last_neo_id))
-
-          mongo_key = ObjectId(json.loads(rec['mongokeys'])['keyframe_rgb'])
-          results = db[collection].find({"_id": mongo_key})
-
+          try: 
+              mongo_key = ObjectId(json.loads(rec['mongokeys'])['keyframe_rgb'])
+              results = db[collection].find({"_id": mongo_key})
+          except Exception, e:
+              print('Skipping frame {}'.format(e))
+              continue
+          
           for item in results:
               im = cv2.imdecode(np.fromstring(str(item['val']), dtype=np.uint8), -1)
-              print im.shape
-              
               label = segnet.extract(im)
-              print label.shape
               
               # Re-encode segnet label
               res, label_encoded = cv2.imencode('.png', label)
@@ -144,27 +150,25 @@ if __name__ == "__main__":
                                                         "description": "Auto-inserted with mongo_interaction.py"})
 
               print 'New mongo key for segnet label', label_monogo_key, type(label_monogo_key)
-              updatemongokeys(session, "SESSTURTLE", last_neo_id, 'keyframe_segnet', str(label_monogo_key))
-    
-
-          
+              updatemongokeys(session, args.session, last_neo_id, 'keyframe_segnet', str(label_monogo_key))
+              time.sleep(0.1)
           
 
-    #####################################################################################
-    ## MongoDB Construction in Python Example
-    # Add a binary image entry (referred to as a "document") to a "collection" in a mongo database.
-    #####################################################################################
+    # #####################################################################################
+    # ## MongoDB Construction in Python Example
+    # # Add a binary image entry (referred to as a "document") to a "collection" in a mongo database.
+    # #####################################################################################
 
-    print "------- GETTING From CG's PNG data ----------"
-    print ""
-
-
-    # collection = "rgb_keyframes"
-
-    # New PNG dataset
-    # key = ObjectId("58ab6b015d76250947c22498")
-    # key = ObjectId("589b7535093d3f1d51cc81da")
+    # print "------- GETTING From CG's PNG data ----------"
+    # print ""
 
 
-    # get the image?
+    # # collection = "rgb_keyframes"
+
+    # # New PNG dataset
+    # # key = ObjectId("58ab6b015d76250947c22498")
+    # # key = ObjectId("589b7535093d3f1d51cc81da")
+
+
+    # # get the image?
 
