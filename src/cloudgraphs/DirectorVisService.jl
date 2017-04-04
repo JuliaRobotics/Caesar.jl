09a10255c0cd91e9af1cad684f779ac85595c051
dehann <dehann.fourie@gmail.com>
2017-04-04 15:00:16 -0400

    new drawing options available with drawdbdirector
    can now draw array array float64 directly

--- src/cloudgraphs/DirectorVisService.jl
+++ src/cloudgraphs/DirectorVisService.jl
@@ -52,36 +52,118 @@ function reconstruct(dc::DepthCamera, depth::Array{Float64})
 end
 
 
-function prepcolordepthcloud!{T <: ColorTypes.Colorant}( X::Array{Float64};
+function prepcolordepthcloud!{T <: ColorTypes.Colorant}( X::Array;
       rgb::Array{T, 2}=Array{Colorant,2}(),
       skip::Int=4,
       maxrange::Float64=4.5 )
   #
-  r,c,h = size(X)
-  Xd = X[1:skip:r,1:skip:c,:]
-  rd,cd,hd = size(Xd)
-  mask = Xd[:,:,:] .> maxrange
-  Xd[mask] = Inf
-
+  pointcloud = nothing
+  pccols = nothing
   havecolor = size(rgb,1) > 0
+  if typeof(X) == Array{Float64,3}
+    r,c,h = size(X)
+    Xd = X[1:skip:r,1:skip:c,:]
+    rd,cd,hd = size(Xd)
+    mask = Xd[:,:,:] .> maxrange
+    Xd[mask] = Inf
 
-  rgbss = havecolor ? rgb[1:skip:r,1:skip:c] : nothing
-  # rgbss = rgb[1:4:r,1:4:c,:]./255.0
-  pts = Vector{Vector{Float64}}()
-  pccols = Vector()
-  for i in 1:rd, j in 1:cd
-    if !isnan(Xd[i,j,1]) && Xd[i,j,3] != Inf
-      push!(pts, vec(Xd[i,j,:]) )
-      havecolor ? push!(pccols, rgbss[i,j] ) : nothing
-      # push!(pccols, RGB(rgbss[i,j,3], rgbss[i,j,2], rgbss[i,j,1]) )
+
+    rgbss = havecolor ? rgb[1:skip:r,1:skip:c] : nothing
+    # rgbss = rgb[1:4:r,1:4:c,:]./255.0
+    pts = Vector{Vector{Float64}}()
+    pccols = Vector()
+    for i in 1:rd, j in 1:cd
+      if !isnan(Xd[i,j,1]) && Xd[i,j,3] != Inf
+        push!(pts, vec(Xd[i,j,:]) )
+        havecolor ? push!(pccols, rgbss[i,j] ) : nothing
+        # push!(pccols, RGB(rgbss[i,j,3], rgbss[i,j,2], rgbss[i,j,1]) )
+      end
     end
+    pointcloud = PointCloud(pts)
+  elseif typeof(X) == Array{Array{Float64,1},1}
+    pointcloud = PointCloud(X)
+  else
+    error("dont know how to deal with data type=$(typeof(X))")
   end
-  pointcloud = PointCloud(pts)
   if havecolor
     pointcloud.channels[:rgb] = pccols
   end
   return pointcloud
 end
+
+function cachepointclouds!(cache::Dict, cv::CloudVertex, ke::AbstractString, dcamjl, imshape)
+  if !haskey(cache, ke)
+    data = getBigDataElement(cv,ke)
+    if typeof(data) == Void
+      # warn("unable to load $(ke) from Mongo, gives data type Void")
+      return nothing
+    end
+    if ke == "depthframe_image"
+      ri,ci = imshape[1], imshape[2] # TODO -- hack should be removed since depth is array and should have rows and columns stored in Mongo
+      arrdata = data.data
+      arr = bin2arr(arrdata, dtype=Float32) # should also store dtype for arr in Mongo
+      img = reshape(arr, ci, ri)'
+      X = reconstruct(dcamjl, Array{Float64,2}(img))
+      cache[ke] = X
+    elseif ke == "pointcloud"
+      buf = IOBuffer(data.data)
+      st = takebuf_string(buf)
+      bb = BSONObject(st)
+      ptarr = map(x -> convert(Array, x), bb["pointcloud"])
+      cache[ke] = ptarr
+    end
+  end
+  nothing
+end
+
+function retrievePointcloudColorInfo!(cv::CloudVertex, va::AbstractString)
+  rgb = Array{Colorant,2}()
+  if !hasBigDataElement(cv, va)
+    warn("could not find color map in mongo, $(va)")
+    return rgb
+  end
+  data = getBigDataElement(cv, va).data
+  if va == "keyframe_rgb" || va == "keyframe_segnet"
+    rgb = ImageMagick.readblob(data);
+  elseif va == "colors"
+    warn("Not implemented yet, $(va)")
+  end
+
+  return rgb
+end
+
+function drawpointcloud!(vis::DrakeVisualizer.Visualizer,
+        poseswithdepth::Dict,
+        vsym::Symbol,
+        pointcloud,
+        va,
+        sesssym::Symbol;
+        imshape=(480,640),
+        wTb::CoordinateTransformations.AbstractAffineMap=
+              Translation(0,0,0.0) ∘ LinearMap(
+              CoordinateTransformations.Quat(1.0, 0, 0, 0)),
+        bTc::CoordinateTransformations.AbstractAffineMap=
+              Translation(0,0,0.6) ∘ LinearMap(
+              CoordinateTransformations.Quat(0.5, -0.5, 0.5, -0.5))  )
+  #
+
+  pcsym = Symbol(string("pc_",va))
+  setgeometry!(vis[sesssym][pcsym][vsym][:pose], Triad())
+  settransform!(vis[sesssym][pcsym][vsym][:pose], wTb) # also updated as parallel track
+  setgeometry!(vis[sesssym][pcsym][vsym][:pose][:cam], Triad())
+  settransform!(vis[sesssym][pcsym][vsym][:pose][:cam], bTc )
+  setgeometry!(vis[sesssym][pcsym][vsym][:pose][:cam][:pc], pointcloud )
+
+  # these poses need to be update if the point cloud is to be moved
+  if !haskey(poseswithdepth,vsym)
+    thetype = typeof(vis[sesssym][pcsym][vsym][:pose])
+    poseswithdepth[vsym] = Vector{ thetype }()
+  end
+  push!(poseswithdepth[vsym], vis[sesssym][pcsym][vsym][:pose])
+
+  nothing
+end
+
 
 function fetchdrawdepthcloudbycvid!(vis::DrakeVisualizer.Visualizer,
       cloudGraph::CloudGraphs.CloudGraph,
@@ -99,51 +181,39 @@ function fetchdrawdepthcloudbycvid!(vis::DrakeVisualizer.Visualizer,
             Translation(0,0,0.6) ∘ LinearMap(
             CoordinateTransformations.Quat(0.5, -0.5, 0.5, -0.5))  )
   #
-  # vsym = Symbol(vert.label)
 
-  # @show vsym
   if !haskey(poseswithdepth, vsym)
     cache = Dict()
-    cv = CloudGraphs.get_vertex(cloudGraph, cvid, true )
+
     # fetch copy of big data from CloudGraphs
-    # "depthframe_image" => "keyframe_rgb", "depthframe_image" => "keyframe_segnet"
+    cv = CloudGraphs.get_vertex(cloudGraph, cvid, true )
+
+    # depthcolormaps = could be one or more or these options
+    # 0, 1 or 2+ color maps per pointcloud
+    #  ("none" => "depthframe_image", "none" => "pointcloud")
+    # or
+    #  ("keyframe_rgb" => "depthframe_image",
+    #  "keyframe_segnet" => "depthframe_image")
+    # or
+    #  ("colors" => "pointcloud")
     for (va, ke) in depthcolormaps
-      ri,ci = imshape[1], imshape[2] # this is a hack # TODO -- should be removed since depth is array and should have rows and columns stored in Mongo
-      if !haskey(cache, ke)
-        data = getBigDataElement(cv,ke)
-        if typeof(data) != Void
-          arrdata = data.data #"depthframe_image"
-          arr = bin2arr(arrdata, dtype=Float32) # should also store dtype for arr in Mongo
-          img = reshape(arr, ci, ri)'
-          X = reconstruct(dcamjl, Array{Float64,2}(img))
-          cache[ke] = X
-        end
-      end
+      # prep the detph pointcloud
+      cachepointclouds!(cache, cv, ke, dcamjl, imshape)
+
       if haskey(cache, ke)
         rgb = Array{Colorant,2}()
-        if hasBigDataElement(cv, va) # "keyframe_rgb"
-          rgbbigd = getBigDataElement(cv, va).data
-          rgb = ImageMagick.readblob(rgbbigd);
+        if va[1:4] != "none"
+          rgb = retrievePointcloudColorInfo!(cv, va)
         end
-        # @show typeof(cache[ke]), typeof(rgb)
+
+        # add color information to the pointcloud
         pointcloud = prepcolordepthcloud!( cache[ke], rgb=rgb )
 
-        pcsym = Symbol(string("pc_",va))
-        setgeometry!(vis[sesssym][pcsym][vsym][:pose], Triad())
-        settransform!(vis[sesssym][pcsym][vsym][:pose], wTb) # also updated as parallel track
-        setgeometry!(vis[sesssym][pcsym][vsym][:pose][:cam], Triad())
-        settransform!(vis[sesssym][pcsym][vsym][:pose][:cam], bTc )
-        setgeometry!(vis[sesssym][pcsym][vsym][:pose][:cam][:pc], pointcloud )
-
-        # these poses need to be update if the point cloud is to be moved
-        if !haskey(poseswithdepth,vsym)
-          thetype = typeof(vis[sesssym][pcsym][vsym][:pose])
-          poseswithdepth[vsym] = Vector{ thetype }()
-        end
-        push!(poseswithdepth[vsym], vis[sesssym][pcsym][vsym][:pose])
+        # publish the pointcloud data to Director viewer
+        drawpointcloud!(vis, poseswithdepth, vsym, pointcloud, va, sesssym, imshape=imshape, wTb=wTb, bTc=bTc)
       end
-    end # for depthcolormaps
-  end # !haskey(poseswithdepth, ke)
+    end
+  end
   nothing
 end
 
@@ -219,7 +289,7 @@ function drawdbsession(vis::DrakeVisualizer.Visualizer,
       poseswithdepth::Dict;
       bTc::CoordinateTransformations.AbstractAffineMap=
             Translation(0,0,0.6) ∘ LinearMap(
-            CoordinateTransformations.Quat(0.5, -0.5, 0.5, -0.5) )     )
+            CoordinateTransformations.Quat(0.5, -0.5, 0.5, -0.5) )    )
   #
 
   @show session = addrdict["session"]
@@ -242,7 +312,7 @@ function drawdbsession(vis::DrakeVisualizer.Visualizer,
     # drawposepoints!(vis, vert, session=session )
   end
 
-  depthcolormaps = Dict("keyframe_rgb" => "depthframe_image", "keyframe_segnet" => "depthframe_image")
+  depthcolormaps = Dict("keyframe_rgb" => "depthframe_image", "keyframe_segnet" => "depthframe_image", "none" => "pointcloud")
 
   @showprogress 1 "Drawing POSE IDs..." for (vid,cvid) in IDs
     fetchdrawposebycvid!(vis,
