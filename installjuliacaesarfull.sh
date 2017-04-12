ca3b2837589f25bafd7bad15f34a61e029045905
dehann <dehann.fourie@gmail.com>
2017-04-12 14:06:56 -0400

    expand ignore and add install script

---
+++ installjuliacaesarfull.sh
@@ -0,0 +1,9 @@
+#!/bin/usr/bash
+
+wget https://julialang.s3.amazonaws.com/bin/linux/x64/0.5/julia-0.5.1-linux-x86_64.tar.gz
+tar -xvf julia-0.5.1-linux-x86_64.tar.gz
+JULIADIR=$(ls --group-directories-first | head -n1)
+./$JULIADIR/bin/julia -O3 -e "Pkg.add(\"Caesar\"); Pkg.checkout(\"Caesar\", \"cloudgraphs\"); Caesar.installcloudgraphs()"
+./$JULIADIR/bin/julia -O3 -e "Pkg.checkout(\"IncrementalInference\"); Pkg.checkout(\"RoME\")"
+./$JULIADIR/bin/julia -O3 -e "using Caesar"
+echo "Julia added a default folder at $HOME/.julia"
