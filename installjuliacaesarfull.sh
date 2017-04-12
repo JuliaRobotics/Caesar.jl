6c96bf44fad87f0218b02101749f461682ceff72
dehann <dehann.fourie@gmail.com>
2017-04-12 15:14:38 -0400

    fix install script

--- installjuliacaesarfull.sh
+++ installjuliacaesarfull.sh
@@ -3,7 +3,7 @@
 wget https://julialang.s3.amazonaws.com/bin/linux/x64/0.5/julia-0.5.1-linux-x86_64.tar.gz
 tar -xvf julia-0.5.1-linux-x86_64.tar.gz
 JULIADIR=$(ls --group-directories-first | head -n1)
-./$JULIADIR/bin/julia -O3 -e "Pkg.add(\"Caesar\"); Pkg.checkout(\"Caesar\", \"cloudgraphs\"); Caesar.installcloudgraphs()"
+./$JULIADIR/bin/julia -O3 -e "Pkg.add(\"Caesar\"); Pkg.checkout(\"Caesar\", \"cloudgraphs\"); using Caesar; Caesar.installcloudgraphs()"
 ./$JULIADIR/bin/julia -O3 -e "Pkg.checkout(\"IncrementalInference\"); Pkg.checkout(\"RoME\")"
 ./$JULIADIR/bin/julia -O3 -e "using Caesar"
 echo "Julia added a default folder at $HOME/.julia"
