5db37533e03df647b7ed50f0478a2737dbb8faaa
dehann <dehann.fourie@gmail.com>
2017-04-28 13:33:09 -0400

    adding some more visualization examples

--- examples/visualization/basicvisualizations.jl
+++ examples/visualization/basicvisualizations.jl
@@ -24,3 +24,39 @@ pointcloud = PointCloud([[x, 0, 0.5] for x in linspace(-1, 1)])
 pointcloud.channels[:rgb] = [RGB(g, (1-g), 0) for g in linspace(0, 1)]
 setgeometry!(vis[:pointcloud], pointcloud)
 delete!(vis[:pointcloud])
+
+
+
+
+
+
+
+
+
+
+#
+using TransformUtils
+
+setgeometry!(vis[:basics][:origin], Triad())
+
+
+q = convert(Quaternion, so3([0.3*pi/2,0,0.0]))
+
+qq = Quat(q.s,q.v...)
+
+setgeometry!(vis[:basics][:triad], Triad())
+settransform!(vis[:basics][:triad], Translation(0.0,0,0) ∘ LinearMap( qq ))
+
+
+for val in linspace(0,pi,100)
+  q = convert(Quaternion, so3([val,0,0.0]))
+  settransform!(vis[:basics][:triad], Translation(0.0,0,0) ∘ LinearMap( Quat(q.s,q.v...) ))
+  @show vee(convert(so3, convert(SO3, q)))
+  sleep(0.05)
+end
+
+
+
+ans = Quaternion(0) * SO3(0) * so3(randn(3)) * TransformUtils.AngleAxis(pi/2, [0,0,1.0])
+
+vee(convert(so3, ans))
