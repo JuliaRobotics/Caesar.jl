6546072636e038e517ca2092b5f1cc4957fb5a03
dgieedelman <dgieedelman@gmail.com>
2017-06-28 15:02:17 -0400

    Moving the Red Box into correct postition

--- src/ModelVisualizationUtils.jl
+++ src/ModelVisualizationUtils.jl
@@ -40,7 +40,7 @@ function loadmodel(model::Symbol=:rov;
     offset = Translation(-0.5,0,0.25) ∘ LinearMap(Rotations.Quat(0,0,0,1.0))
     return DrawROV(rovdata, 99, :rov, offset)
   elseif model == :scene01
-    boxdata = GeometryData(HyperRectangle(Vec(0.0,4.0,-0.7), Vec(5.0,5.0,1.4)), RGBA(0.5,0.1,0.0,0.5))
+    boxdata = GeometryData(HyperRectangle(Vec(4.0,0,-0.7), Vec(5.0,5.0,1.4)), RGBA(0.5,0.1,0.0,0.5))
     # boxdata.color =
     offset = Translation(0.0,0,0) ∘ LinearMap(CoordinateTransformations.AngleAxis(0.0,0,0,1.0))
     return DrawScene(boxdata, :scene01, offset)
