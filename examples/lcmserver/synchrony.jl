cec8bfe37c94b5b457e73e2ed63b162722e033ad
dehann <dehann.fourie@gmail.com>
2018-04-18 14:33:37 -0400

    mostly reworked hauv example but Syncr has not been updated yet,
    code not tested and waiting on Syncr development before more work can continue on this file

--- examples/lcmserver/synchrony.jl
+++ examples/lcmserver/synchrony.jl
@@ -13,7 +13,11 @@ using LibBSON
 
 using RobotTestDatasets
 
+"""
+    CaesarSLAM
 
+An object definition containing the require variables to leverage the server side SLAM solution per user, robot, and session.
+"""
 mutable struct CaesarSLAM
   userId::AbstractString
   robotId::AbstractString
@@ -129,32 +133,15 @@ function initialize!(cslaml::CaesarSLAM;
 end
 
 
-# create a SLAM container object
-cslam = CaesarSLAM(userId=userId,robotId=robotId,sessionId=sessionId)
 
-# initialize a new session ready for SLAM using the built in SynchronySDK
-initialize!(cslam)
+# TODO -- code below untested
 
+"""
+    setRobotParameters!(::CaesarSLAM)
 
-# TEMPORARY CHECK THAT THNGS ARE WORKING (albeit 2D / need 3D)
-for i in 0:5
-    deltaMeasurement = [10.0;0;pi/3]
-    pOdo = Float64[[0.1 0 0] [0 0.1 0] [0 0 0.1]]
-    println(" - Measurement $i: Adding new odometry measurement '$deltaMeasurement'...")
-    newOdometryMeasurement = AddOdometryRequest(deltaMeasurement, pOdo)
-    @time odoResponse = addOdometryMeasurement(cslam.syncrconf, cslam.robotId, cslam.sessionId, newOdometryMeasurement)
-end
-
-
-# TODO -- MUST UPDATE Synchr to allow 3D and not auto create x0 with prior since 2D or 3D are very different
-
-
-
-# OLD CLOUD GRAPHS CODE STILL TO BE CONVERTED BELOW
-
-
-
-function setRobotParameters!(cg::CloudGraph, session::AbstractString)
+Store robot parameters in the centralized database system.
+"""
+function setRobotParameters!(cslaml::CaesarSLAM)
   hauvconfig = Dict()
   hauvconfig["robot"] = "hauv"
   hauvconfig["bTc"] = [0.0;0.0;0.0; 1.0; 0.0; 0.0; 0.0]
@@ -170,16 +157,18 @@ function setRobotParameters!(cg::CloudGraph, session::AbstractString)
 end
 
 """
+    handle_poses!(::CaesarSLAM, ::pose_node_t)
+
 Adds pose nodes to graph with a prior on Z, pitch, and roll.
 """
-function handle_poses!(slam::SLAMWrapper,
-                       message_data)
-    message = caesar.pose_node_t[:decode](message_data)
-    id = message[:id]
-    println("[Caesar.jl] Received pose message for x$(id)")
+function handle_poses!(slaml::CaesarSLAM,
+                       msg::pose_node_t)
+    #
+    id = msg.id
+    println("[Caesar.jl] Received pose msg for x$(id)")
 
-    mean = message[:mean]
-    covar = message[:covar]
+    mean = msg.mean
+    covar = msg.covar
     t = [mean[1], mean[2], mean[3]]
     qw = mean[4]
     qxyz = [mean[5], mean[6], mean[7]]
@@ -188,102 +177,110 @@ function handle_poses!(slam::SLAMWrapper,
     euler = Euler(q)
 
     node_label = Symbol("x$(id)")
-    xn = addNode!(slam.fg, node_label, labels=["POSE"], dims=6) # this is an incremental inference call
-    slam.lastposesym = node_label; # update object
+    xn = addNode!(slaml, node_label, labels=["POSE"], dims=6) # this is an incremental inference call
+    slaml.lastposesym = node_label; # update object
 
     if id == 1
         println("[Caesar.jl] First pose")
-        # this is the first message, and it does not carry odometry, but the prior on the first node.
+        # this is the first msg, and it does not carry odometry, but the prior on the first node.
 
         # add 6dof prior
         initPosePrior = PriorPose3( MvNormal( veeEuler(pose), diagm([covar...]) ) )
-        addFactor!(slam.fg, [xn], initPosePrior)
+        addFactor!(slaml, [xn], initPosePrior)
 
         # auto init is coming, this code will likely be removed
-        initializeNode!(slam.fg, node_label)
+        initializeNode!(slaml, node_label)
 
         # set robot parameters in the first pose, this will become a separate node in the future
         println("[Caesar.jl] Setting robot parameters")
-        setRobotParameters!(slam.fg.cg, slam.fg.sessionname)
+        setRobotParameters!(slaml)
     end
 end
 
-# handles ZPR priors on poses
-function handle_priors!(slam::SLAMWrapper,
-                         message_data)
+"""
+    handle_priors!(::CaesarSLAM, ::prior_zpr_t)
 
+Handle ZPR priors on poses.
+"""
+function handle_priors!(slam::CaesarSLAM,
+                         msg::prior_zpr_t)
 
-    message = caesar.prior_zpr_t[:decode](message_data)
-    id = message[:id]
+    id = msg.id
     println("[Caesar.jl] Adding prior on RPZ to x$(id)")
 
     node_label = Symbol("x$(id)")
-    xn = getVert(slam.fg,node_label)
+    xn = getVert(slam,node_label)
 
-    z = message[:z]
-    pitch = message[:pitch]
-    roll = message[:roll]
+    z = msg.z
+    pitch = msg.pitch
+    roll = msg.roll
 
-    var_z = message[:var_z]
-    var_pitch = message[:var_pitch]
-    var_roll = message[:var_roll]
+    var_z = msg.var_z
+    var_pitch = msg.var_pitch
+    var_roll = msg.var_roll
 
     rp_dist = MvNormal( [roll;pitch], diagm([var_roll, var_pitch]))
     z_dist = Normal(z, var_z)
     prior_rpz = PartialPriorRollPitchZ(rp_dist, z_dist)
-    addFactor!(slam.fg, [xn], prior_rpz)
+    addFactor!(slam, [xn], prior_rpz)
 end
 
+"""
+    handle_partials!(::CaesarSLAM, ::pose_pose_xyh_t)
 
-function handle_partials!(slam::SLAMWrapper,
-                         message_data)
-    message = caesar.pose_pose_xyh_t[:decode](message_data)
+Handle partial x, y, and heading odometry constraints between Pose3 variables.
+"""
+function handle_partials!(slam::CaesarSLAM,
+                         msg::pose_pose_xyh_t)
 
-    origin_id = message[:node_1_id]
-    destination_id = message[:node_2_id]
+    origin_id = msg.node_1_id
+    destination_id = msg.node_2_id
     origin_label = Symbol("x$(origin_id)")
     destination_label = Symbol("x$(destination_id)")
 
     println("[Caesar.jl] Adding XYH odometry constraint between(x$(origin_id), x$(destination_id))")
 
-    delta_x = message[:delta_x]
-    delta_y = message[:delta_y]
-    delta_yaw = message[:delta_yaw]
+    delta_x = msg.delta_x
+    delta_y = msg.delta_y
+    delta_yaw = msg.delta_yaw
 
-    var_x = message[:var_x]
-    var_y = message[:var_y]
-    var_yaw = message[:var_yaw]
+    var_x = msg.var_x
+    var_y = msg.var_y
+    var_yaw = msg.var_yaw
 
     origin_label, destination_label
-    xo = getVert(slam.fg,origin_label)
-    xd = getVert(slam.fg,destination_label)
+    xo = getVert(slam,origin_label)
+    xd = getVert(slam,destination_label)
 
     xyh_dist = MvNormal([delta_x, delta_y, delta_yaw], diagm([var_x, var_y, var_yaw]))
     xyh_factor = PartialPose3XYYaw(xyh_dist)
-    addFactor!(slam.fg, [xo;xd], xyh_factor)
+    addFactor!(slam, [xo;xd], xyh_factor)
 
-    initializeNode!(slam.fg, destination_label)
+    initializeNode!(slam, destination_label)
     println()
 end
 
+"""
+   handle_loops!(::CaesarSLAM, ::pose_pose_xyh_nh_t)
 
-function handle_loops!(slaml::SLAMWrapper,
-                       message_data)
-    message = caesar.pose_pose_xyh_nh_t[:decode](message_data)
+Handle loop closure proposals with chance of being a null hypothesis likelihood.
+"""
+function handle_loops!(slaml::CaesarSLAM,
+                       msg::pose_pose_xyh_nh_t)
 
-    origin_id = message[:node_1_id]
-    destination_id = message[:node_2_id]
+    origin_id = msg.node_1_id
+    destination_id = msg.node_2_id
     origin_label = Symbol("x$(origin_id)")
     destination_label = Symbol("x$(destination_id)")
 
-    delta_x = message[:delta_x]
-    delta_y = message[:delta_y]
-    delta_yaw = message[:delta_yaw]
+    delta_x = msg.delta_x
+    delta_y = msg.delta_y
+    delta_yaw = msg.delta_yaw
 
-    var_x = message[:var_x]
-    var_y = message[:var_y]
-    var_yaw = message[:var_yaw]
-    confidence = message[:confidence]
+    var_x = msg.var_x
+    var_y = msg.var_y
+    var_yaw = msg.var_yaw
+    confidence = msg.confidence
 
     xo = getVert(slaml.fg,origin_label)
     xd = getVert(slaml.fg,destination_label)
@@ -296,7 +293,7 @@ function handle_loops!(slaml::SLAMWrapper,
     println("[Caesar.jl] Adding XYH-NH loop closure constraint between (x$(origin_id), x$(destination_id))")
     xyh_dist = MvNormal([delta_x, delta_y, delta_yaw], diagm([var_x, var_y, var_yaw]))
     xyh_factor = PartialPose3XYYawNH(xyh_dist ,[1.0-confidence, confidence]) # change to NH
-    addFactor!(slaml.fg, [xo;xd], xyh_factor )
+    addFactor!(slaml, [xo;xd], xyh_factor )
 
     # println("[Caesar.jl] Adding P3P3NH loop closure constraint between (x$(origin_id), x$(destination_id))")
 
@@ -304,7 +301,7 @@ function handle_loops!(slaml::SLAMWrapper,
     # lcf = Pose3Pose3NH( MvNormal(veeEuler(rel_pose), diagm(1.0./covar)), [0.5;0.5]) # define 50/50% hypothesis
     # lcf_label = Symbol[origin_label;destination_label]
 
-    # addFactor!(slaml.fg, lcf_label, lcf)
+    # addFactor!(slaml, lcf_label, lcf)
 end
 
 
@@ -313,7 +310,7 @@ end
 #     colors::
 # end
 
-# function add_pointcloud!(slaml::SLAMWrapper, nodeID::Symbol, cloud::Cloud )
+# function add_pointcloud!(slaml::CaesarSLAM, nodeID::Symbol, cloud::Cloud )
 #     # fetch from database
 #     vert = getVert(slaml.fg, nodeID, api=IncrementalInference.dlapi)
 
@@ -329,42 +326,39 @@ end
 
 
 """
-   handle_clouds(slam::SLAMWrapper, message_data)
+   handle_clouds(slam::CaesarSLAM, msg_data)
 
-   Callback for caesar_point_cloud_t messages. Adds point cloud to SLAM_Client
+   Callback for caesar_point_cloud_t msgs. Adds point cloud to SLAM_Client
 """
-function handle_clouds!(slam::SLAMWrapper,
-                        message_data)
+function handle_clouds!(slaml::CaesarSLAM,
+                        msg::point_cloud_t)
     # TODO: interface here should be as simple as slam_client.add_pointcloud(nodeID, pc::SomeCloudType)
 
     # TODO: check for empty clouds!
 
-    message = caesar.point_cloud_t[:decode](message_data)
-
-    id = message[:id]
+    id = msg.id
 
     last_pose = Symbol("x$(id)")
     println("[Caesar.jl] Got cloud $id")
 
     # 2d arrays of points and colors (from LCM data into arrays{arrays})
-    points = [[pt[1], pt[2], pt[3]] for pt in message[:points]]
-    colors = [[UInt8(c.data[1]),UInt8(c.data[2]),UInt8(c.data[3])] for c in message[:colors]]
+    points = [[pt[1], pt[2], pt[3]] for pt in msg.points]
+    colors = [[UInt8(c.data[1]),UInt8(c.data[2]),UInt8(c.data[3])] for c in msg.colors]
 
 
-    # TODO: check if vert exists or not (may happen if messages are lost or out of order)
-    vert = getVert(slam.fg, last_pose, api=IncrementalInference.dlapi) # fetch from database
+    # TODO: check if vert exists or not (may happen if msgs are lost or out of order)
+    vert = getVert(slaml, last_pose, api=IncrementalInference.dlapi) # fetch from database
 
     # push to mongo (using BSON as a quick fix)
     # (for deserialization, see src/DirectorVisService.jl:cachepointclouds!)
     serialized_point_cloud = BSONObject(Dict("pointcloud" => points))
-    appendvertbigdata!(slam.fg, vert, "BSONpointcloud", string(serialized_point_cloud).data)
+    appendvertbigdata!(slaml, vert, "BSONpointcloud", string(serialized_point_cloud).data)
     serialized_colors = BSONObject(Dict("colors" => colors))
-    appendvertbigdata!(slam.fg, vert, "BSONcolors", string(serialized_colors).data)
+    appendvertbigdata!(slaml, vert, "BSONcolors", string(serialized_colors).data)
 end
 
-# this function handles lcm messages
-function listener!(slam::SLAMWrapper,
-                   lcm_node::LCMCore.LCM)
+# this function handles lcm msgs
+function listener!(lcm_node::Union{LCMCore.LCM, LCMCore.LCMLog})
     # handle traffic
     # TODO: handle termination
     while true
@@ -375,22 +369,18 @@ end
 
 # 0. Constants
 println("[Caesar.jl] defining constants.")
-data = robotdata("rovlcm_singlesession_01")
 userId = "ROVUser"
 robotId = "HAUV"
 sessionId = "LCM_01"
 
+# create a SLAM container object
+slam_client = CaesarSLAM(userId=userId,robotId=robotId,sessionId=sessionId)
 
-#@load "usercfg.jld"
-#user_config["session"] = "SESSHAUVDEV3"
-@load "liljon17.jld"
-#  include(joinpath(dirname(@__FILE__),"..","database","blandauthremote.jl"))
-# user_config = addrdict
-backend_config, user_config = standardcloudgraphsetup(addrdict=user_config)
+# initialize a new session ready for SLAM using the built in SynchronySDK
+println("[Caesar.jl] Setting up remote solver")
+initialize!(slam_client)
 
-
-println("[Caesar.jl] Setting up local solver")
-slam_client = initialize!(backend_config,user_config)
+# TODO - should have a function that allows first pose and prior to be set by user.
 
 # create new handlers to pass in additional data
 lcm_pose_handler = (channel, message_data) -> handle_poses!(slam_client, message_data )
@@ -400,15 +390,20 @@ lcm_cloud_handler = (channel, message_data) -> handle_clouds!(slam_client, messa
 lcm_loop_handler = (channel, message_data) -> handle_loops!(slam_client, message_data )
 
 # create LCM object and subscribe to messages on the following channels
-lcm_node = LCM()
+logfile = robotdata("rovlcm_singlesession_01")
+lcm_node = LCMLog(logfile) # for direct log file access
+# lcm_node = LCM() # for UDP Ethernet traffic version
+
 # poses
-subscribe(lcm_node, "CAESAR_POSES", lcm_pose_handler)
+subscribe(lcm_node, "CAESAR_POSES", lcm_pose_handler, pose_pose_nh_t)
 # factors
-subscribe(lcm_node, "CAESAR_PARTIAL_XYH", lcm_odom_handler)
-subscribe(lcm_node, "CAESAR_PARTIAL_ZPR", lcm_prior_handler)
-subscribe(lcm_node, "CAESAR_PARTIAL_XYH_NH", lcm_loop_handler) # loop closures come in via p3p3nh factors
+subscribe(lcm_node, "CAESAR_PARTIAL_XYH", lcm_odom_handler, pose_pose_xyh_t)
+subscribe(lcm_node, "CAESAR_PARTIAL_ZPR", lcm_prior_handler, prior_zpr_t)
+# loop closures come in via p3p3nh factors
+subscribe(lcm_node, "CAESAR_PARTIAL_XYH_NH", lcm_loop_handler, pose_pose_xyh_nh_t)
 # sensor data
-subscribe(lcm_node, "CAESAR_POINT_CLOUDS", lcm_cloud_handler)
+subscribe(lcm_node, "CAESAR_POINT_CLOUDS", lcm_cloud_handler, point_cloud_t)
+
 
 println("[Caesar.jl] Running LCM listener")
-listener!(slam_client, lcm_node)
+listener!(lcm_node)
