3f6ee44865999f7b58dd16a14f7777e27887f2e9
GearsAD <sclaassens.ad@gmail.com>
2018-05-02 23:00:49 -0500

    Loop closures now ingesting correctly

---
+++ examples/marine/rov/highend/lcmHandlers.jl
@@ -0,0 +1,173 @@
+"""
+Adds pose nodes to graph with a prior on Z, pitch, and roll.
+"""
+function handle_poses!(slam::SyncrSLAM,
+                       msg::pose_node_t)
+    id = msg.id
+    println("[Caesar.jl] Received pose msg for x$(id)")
+
+    mean = msg.mean
+    covar = msg.covar
+    t = [mean[1], mean[2], mean[3]]
+    qw = mean[4]
+    qxyz = [mean[5], mean[6], mean[7]]
+    q = Quaternion(qw,qxyz) # why not a (w,x,y,z) constructor?
+    pose = SE3(t,q)
+    euler = Euler(q)
+    #
+    node_label = Symbol("x$(id)")
+    varRequest = VariableRequest(node_label, "Pose3", nothing, ["POSE"])
+    resp = addVariable(slam.syncrconf, slam.robotId, slam.sessionId, varRequest)
+    # xn = addNode!(slaml, node_label, labels=["POSE"], dims=6) # this is an incremental inference call
+    # slaml.lastposesym = node_label; # update object
+    #
+    if id == 0
+        println("[Caesar.jl] First pose")
+        # this is the first msg, and it does not carry odometry, but the prior on the first node.
+
+        # add 6dof prior
+        initPosePrior = RoME.PriorPose3( MvNormal( veeEuler(pose), diagm([covar...]) ) )
+        packedPrior = convert(RoME.PackedPriorPose3, initPosePrior)
+
+        # 3. Build the factor request (again, we can make this way easier and transparent once it's stable)
+        fctBody = FactorBody(string(typeof(initPosePrior)), string(typeof(packedPrior)), "JSON", JSON.json(packedPrior))
+        fctRequest = FactorRequest([node_label], fctBody, false, false)
+        @show resp = addFactor(slam.syncrconf, slam.robotId, slam.sessionId, fctRequest)
+
+        # auto init is coming, this code will likely be removed
+        # initializeNode!(slaml, node_label)
+
+        # set robot parameters in the first pose, this will become a separate node in the future
+        println("[Caesar.jl] Setting robot parameters")
+        setRobotParameters!(slam)
+    end
+end
+
+"""
+Handle ZPR priors on poses.
+"""
+function handle_priors!(slam::SyncrSLAM,
+                         msg::prior_zpr_t)
+
+    id = msg.id
+    println("[Caesar.jl] Adding prior on RPZ to x$(id)")
+
+    node_label = Symbol("x$(id)")
+    # xn = getVert(slam,node_label)
+
+    # 1. Build up the prior
+    z = msg.z
+    pitch = msg.pitch
+    roll = msg.roll
+    var_z = msg.var_z
+    var_pitch = msg.var_pitch
+    var_roll = msg.var_roll
+    rp_dist = MvNormal( [roll;pitch], diagm([var_roll, var_pitch]))
+    z_dist = Normal(z, var_z)
+    prior_rpz = RoME.PartialPriorRollPitchZ(rp_dist, z_dist)
+
+    # 2. Pack the prior (we can automate this step soon, but for now it's hand cranking)
+    packed_prior_rpz = convert(RoME.PackedPartialPriorRollPitchZ, prior_rpz)
+
+    # 3. Build the factor request (again, we can make this way easier and transparent once it's stable)
+    # fctBody = FactorBody(string(typeof(prior_rpz)), string(typeof(packed_prior_rpz)), "JSON", JSON.json(packed_prior_rpz))
+    # fctRequest = FactorRequest([node_label], fctBody, false, false)
+    # @show resp = addFactor(slam.syncrconf, slam.robotId, slam.sessionId, fctRequest)
+end
+
+"""
+Handle partial x, y, and heading odometry constraints between Pose3 variables.
+"""
+function handle_partials!(slam::SyncrSLAM,
+                         msg::pose_pose_xyh_t)
+    println(" --- Handling odometry change...")
+    return
+
+    origin_id = msg.node_1_id
+    destination_id = msg.node_2_id
+    origin_label = Symbol("x$(origin_id)")
+    destination_label = Symbol("x$(destination_id)")
+
+    println("[Caesar.jl] Adding XYH odometry constraint between(x$(origin_id), x$(destination_id))")
+
+    delta_x = msg.delta_x
+    delta_y = msg.delta_y
+    delta_yaw = msg.delta_yaw
+
+    var_x = msg.var_x
+    var_y = msg.var_y
+    var_yaw = msg.var_yaw
+
+    origin_label, destination_label
+    # @show xo = getVert(slam,origin_label)
+    # @show xd = getVert(slam,destination_label)
+
+    xyh_dist = MvNormal([delta_x, delta_y, delta_yaw], diagm([var_x, var_y, var_yaw]))
+    xyh_factor = PartialPose3XYYaw(xyh_dist)
+    # 2. Pack the prior (we can automate this step soon, but for now it's hand cranking)
+    packed_xyh_factor = convert(RoME.PackedPartialPose3XYYaw, xyh_factor)
+
+    # 3. Build the factor request (again, we can make this way easier and transparent once it's stable)
+    fctBody = FactorBody(string(typeof(xyh_factor)), string(typeof(packed_xyh_factor)), "JSON", JSON.json(packed_xyh_factor))
+    fctRequest = FactorRequest([origin_label; destination_label], fctBody, false, false)
+    @show resp = addFactor(slam.syncrconf, slam.robotId, slam.sessionId, fctRequest)
+
+    # addFactor!(slam, [xo;xd], xyh_factor)
+    #
+    # initializeNode!(slam, destination_label)
+    # println()
+end
+
+"""
+Handle loop closure proposals with chance of being a null hypothesis likelihood.
+"""
+function handle_loops!(slam::SyncrSLAM,
+                       msg::pose_pose_xyh_nh_t)
+
+    println(" --- Handling loop...")
+    # return nothing
+
+    origin_id = msg.node_1_id
+    destination_id = msg.node_2_id
+    origin_label = Symbol("x$(origin_id)")
+    destination_label = Symbol("x$(destination_id)")
+
+    delta_x = msg.delta_x
+    delta_y = msg.delta_y
+    delta_yaw = msg.delta_yaw
+
+    var_x = msg.var_x
+    var_y = msg.var_y
+    var_yaw = msg.var_yaw
+    confidence = msg.confidence
+
+    # @show xo = getVert(slam.fg,origin_label)
+    # @show xd = getVert(slam.fg,destination_label)
+
+    # if (destination_id - origin_id == 1)
+    #     warn("Avoiding parallel factor! See: https://github.com/dehann/IncrementalInference.jl/issues/63To ")
+    #     return
+    # end
+
+    println("[Caesar.jl] Adding XYH-NH loop closure constraint between (x$(origin_id), x$(destination_id))")
+    xyh_dist = MvNormal([delta_x, delta_y, delta_yaw], diagm([var_x, var_y, var_yaw]))
+    xyh_factor = RoME.PartialPose3XYYawNH(xyh_dist ,[1.0-confidence, confidence]) # change to NH
+    # 2. Pack the prior (we can automate this step soon, but for now it's hand cranking)
+    packed_xyh_factor = convert(RoME.PackedPartialPose3XYYawNH, xyh_factor)
+
+    # 3. Build the factor request (again, we can make this way easier and transparent once it's stable)
+    fctBody = FactorBody(string(typeof(xyh_factor)), string(typeof(packed_xyh_factor)), "JSON", JSON.json(packed_xyh_factor))
+    fctRequest = FactorRequest([origin_label; destination_label], fctBody, false, false)
+    @show resp = addFactor(slam.syncrconf, slam.robotId, slam.sessionId, fctRequest)
+
+    # addFactor!(slaml, [xo;xd], xyh_factor )
+
+    # NOT USED
+    # println("[Caesar.jl] Adding P3P3NH loop closure constraint between (x$(origin_id), x$(destination_id))")
+
+    # # line below fails!
+    # lcf = Pose3Pose3NH( MvNormal(veeEuler(rel_pose), diagm(1.0./covar)), [0.5;0.5]) # define 50/50% hypothesis
+    # lcf_label = Symbol[origin_label;destination_label]
+
+    # addFactor!(slaml, lcf_label, lcf)
+end
--- examples/marine/rov/highend/synchrony.jl
+++ examples/marine/rov/highend/synchrony.jl
@@ -1,6 +1,5 @@
 #=
 LCM Server: an LCM interface to Caesar.jl
-
 =#
 
 using Caesar
@@ -34,100 +33,22 @@ function setRobotParameters!(sslaml::SyncrSLAM)
   nothing
 end
 
-
-# TODO -- code below untested
-
-"""
-Adds pose nodes to graph with a prior on Z, pitch, and roll.
-"""
-function handle_poses!(slam::SyncrSLAM,
-                       msg::pose_node_t)
-    id = msg.id
-    println("[Caesar.jl] Received pose msg for x$(id)")
-
-    mean = msg.mean
-    covar = msg.covar
-    t = [mean[1], mean[2], mean[3]]
-    qw = mean[4]
-    qxyz = [mean[5], mean[6], mean[7]]
-    q = Quaternion(qw,qxyz) # why not a (w,x,y,z) constructor?
-    pose = SE3(t,q)
-    euler = Euler(q)
-    #
-    node_label = Symbol("x$(id)")
-    varRequest = VariableRequest(node_label, "Pose3", nothing, ["POSE"])
-    resp = addVariable(slam.syncrconf, slam.robotId, slam.sessionId, varRequest)
-    # xn = addNode!(slaml, node_label, labels=["POSE"], dims=6) # this is an incremental inference call
-    # slaml.lastposesym = node_label; # update object
-    #
-    if id == 0
-        println("[Caesar.jl] First pose")
-        # this is the first msg, and it does not carry odometry, but the prior on the first node.
-
-        # add 6dof prior
-        initPosePrior = RoME.PriorPose3( MvNormal( veeEuler(pose), diagm([covar...]) ) )
-        packedPrior = convert(RoME.PackedPriorPose3, initPosePrior)
-
-        # 3. Build the factor request (again, we can make this way easier and transparent once it's stable)
-        fctBody = FactorBody(string(typeof(initPosePrior)), string(typeof(packedPrior)), "JSON", JSON.json(packedPrior))
-        fctRequest = FactorRequest([node_label], fctBody, false, false)
-        @show resp = addFactor(slam.syncrconf, slam.robotId, slam.sessionId, fctRequest)
-
-        # auto init is coming, this code will likely be removed
-        # initializeNode!(slaml, node_label)
-
-        # set robot parameters in the first pose, this will become a separate node in the future
-        println("[Caesar.jl] Setting robot parameters")
-        setRobotParameters!(slam)
-    end
-end
-
-"""
-Handle ZPR priors on poses.
-"""
-function handle_priors!(slam::SyncrSLAM,
-                         msg::prior_zpr_t)
-
-    id = msg.id
-    println("[Caesar.jl] Adding prior on RPZ to x$(id)")
-
-    node_label = Symbol("x$(id)")
-    # xn = getVert(slam,node_label)
-
-    # 1. Build up the prior
-    z = msg.z
-    pitch = msg.pitch
-    roll = msg.roll
-    var_z = msg.var_z
-    var_pitch = msg.var_pitch
-    var_roll = msg.var_roll
-    rp_dist = MvNormal( [roll;pitch], diagm([var_roll, var_pitch]))
-    z_dist = Normal(z, var_z)
-    prior_rpz = RoME.PartialPriorRollPitchZ(rp_dist, z_dist)
-
-    # 2. Pack the prior (we can automate this step soon, but for now it's hand cranking)
-    packed_prior_rpz = convert(RoME.PackedPartialPriorRollPitchZ, prior_rpz)
-
-    # 3. Build the factor request (again, we can make this way easier and transparent once it's stable)
-    fctBody = FactorBody(string(typeof(prior_rpz)), string(typeof(packed_prior_rpz)), "JSON", JSON.json(packed_prior_rpz))
-    fctRequest = FactorRequest([node_label], fctBody, false, false)
-    @show resp = addFactor(slam.syncrconf, slam.robotId, slam.sessionId, fctRequest)
-end
+# Include the LCM handlers...
+include(joinpath(dirname(@__FILE__), "lcmHandlers.jl"))
 
 # this function handles lcm msgs
 function listener!(lcm_node::Union{LCMCore.LCM, LCMCore.LCMLog})
     # handle traffic
-    # TODO: handle termination
+        # TODO: handle termination
     while true
         handle(lcm_node)
     end
 end
 
-
 # 0. Constants
 println("[Caesar.jl] defining constants.")
 robotId = "HROV"
-sessionId = "LCM_17"
+sessionId = "LCM_27"
 
 # create a SLAM container object
 slam_client = SyncrSLAM(robotId, sessionId, nothing)
@@ -150,10 +71,10 @@ robotConfig = Syncr.getRobotConfig(slam_client.syncrconf, slam_client.robotId)
 
 # create new handlers to pass in additional data
 lcm_pose_handler = (channel, message_data) -> handle_poses!(slam_client, message_data )
-# lcm_odom_handler = (channel, message_data) -> handle_partials!(slam_client, message_data )
+lcm_odom_handler = (channel, message_data) -> handle_partials!(slam_client, message_data )
 lcm_prior_handler = (channel, message_data) -> handle_priors!(slam_client, message_data )
 # lcm_cloud_handler = (channel, message_data) -> handle_clouds!(slam_client, message_data )
-# lcm_loop_handler = (channel, message_data) -> handle_loops!(slam_client, message_data )
+lcm_loop_handler = (channel, message_data) -> handle_loops!(slam_client, message_data )
 
 # create LCM object and subscribe to messages on the following channels
 logfile = robotdata("rovlcm_singlesession_01")
@@ -163,10 +84,10 @@ lcm_node = LCMLog(logfile) # for direct log file access
 subscribe(lcm_node, "CAESAR_POSES", lcm_pose_handler, pose_node_t)
 
 # factors
-# subscribe(lcm_node, "CAESAR_PARTIAL_XYH", lcm_odom_handler, pose_pose_xyh_t)
-subscribe(lcm_node, "CAESAR_PARTIAL_ZPR", lcm_prior_handler, prior_zpr_t)
+subscribe(lcm_node, "*CAESAR_PARTIAL_XYH*", lcm_odom_handler, pose_pose_xyh_t)
+# subscribe(lcm_node, "CAESAR_PARTIAL_ZPR", lcm_prior_handler, prior_zpr_t)
 # loop closures come in via p3p3nh factors
-# subscribe(lcm_node, "CAESAR_PARTIAL_XYH_NH", lcm_loop_handler, pose_pose_xyh_nh_t)
+subscribe(lcm_node, "CAESAR_PARTIAL_XYH_NH", lcm_loop_handler, pose_pose_xyh_nh_t)
 
 # sensor data
 # subscribe(lcm_node, "CAESAR_POINT_CLOUDS", lcm_cloud_handler, point_cloud_t)
@@ -178,87 +99,14 @@ listener!(lcm_node)
 ########### TESTING ################
 ####################################
 
-
-
-
+using Unmarshal
+packedJson = "{\"vecZij\":[-0.3171846270561218,-7.061707019805908,-0.14232116187023247],\"vecCov\":[0.01,0.0,0.0,0.0,0.01,0.0,0.0,0.0,0.01],\"nullhypothesis\":[0.5,0.5]}"
+dictPacked = JSON.parse(packedJson)
+packedType = Unmarshal.unmarshal(RoME.PackedPartialPose3XYYawNH, dictPacked)
+prior = convert(RoME.PartialPose3XYYawNH, packedType)
 ####################################
 #### TODO stuff
 ####################################
-
-"""
-Handle partial x, y, and heading odometry constraints between Pose3 variables.
-"""
-function handle_partials!(slam::SyncrSLAM,
-                         msg::pose_pose_xyh_t)
-
-    origin_id = msg.node_1_id
-    destination_id = msg.node_2_id
-    origin_label = Symbol("x$(origin_id)")
-    destination_label = Symbol("x$(destination_id)")
-
-    println("[Caesar.jl] Adding XYH odometry constraint between(x$(origin_id), x$(destination_id))")
-
-    delta_x = msg.delta_x
-    delta_y = msg.delta_y
-    delta_yaw = msg.delta_yaw
-
-    var_x = msg.var_x
-    var_y = msg.var_y
-    var_yaw = msg.var_yaw
-
-    origin_label, destination_label
-    xo = getVert(slam,origin_label)
-    xd = getVert(slam,destination_label)
-
-    xyh_dist = MvNormal([delta_x, delta_y, delta_yaw], diagm([var_x, var_y, var_yaw]))
-    xyh_factor = PartialPose3XYYaw(xyh_dist)
-    addFactor!(slam, [xo;xd], xyh_factor)
-
-    initializeNode!(slam, destination_label)
-    println()
-end
-
-"""
-Handle loop closure proposals with chance of being a null hypothesis likelihood.
-"""
-function handle_loops!(slaml::SyncrSLAM,
-                       msg::pose_pose_xyh_nh_t)
-
-    origin_id = msg.node_1_id
-    destination_id = msg.node_2_id
-    origin_label = Symbol("x$(origin_id)")
-    destination_label = Symbol("x$(destination_id)")
-
-    delta_x = msg.delta_x
-    delta_y = msg.delta_y
-    delta_yaw = msg.delta_yaw
-
-    var_x = msg.var_x
-    var_y = msg.var_y
-    var_yaw = msg.var_yaw
-    confidence = msg.confidence
-
-    xo = getVert(slaml.fg,origin_label)
-    xd = getVert(slaml.fg,destination_label)
-
-    # if (destination_id - origin_id == 1)
-    #     warn("Avoiding parallel factor! See: https://github.com/dehann/IncrementalInference.jl/issues/63To ")
-    #     return
-    # end
-
-    println("[Caesar.jl] Adding XYH-NH loop closure constraint between (x$(origin_id), x$(destination_id))")
-    xyh_dist = MvNormal([delta_x, delta_y, delta_yaw], diagm([var_x, var_y, var_yaw]))
-    xyh_factor = PartialPose3XYYawNH(xyh_dist ,[1.0-confidence, confidence]) # change to NH
-    addFactor!(slaml, [xo;xd], xyh_factor )
-
-    # println("[Caesar.jl] Adding P3P3NH loop closure constraint between (x$(origin_id), x$(destination_id))")
-
-    # # line below fails!
-    # lcf = Pose3Pose3NH( MvNormal(veeEuler(rel_pose), diagm(1.0./covar)), [0.5;0.5]) # define 50/50% hypothesis
-    # lcf_label = Symbol[origin_label;destination_label]
-
-    # addFactor!(slaml, lcf_label, lcf)
-end
 
 
 # type Cloud
