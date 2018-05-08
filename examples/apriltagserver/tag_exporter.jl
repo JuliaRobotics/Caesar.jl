988a987e84c9e3b3b3cda192e7ae8e3d8b28220a
dgieedelman <dgieedelman@gmail.com>
2018-05-08 16:11:45 -0400

    tags drawing better in the world frame

--- examples/apriltagserver/tag_exporter.jl
+++ examples/apriltagserver/tag_exporter.jl
@@ -23,7 +23,7 @@ function apriltag_converter(a::AprilTag)
     msg.p=P
     msg.cxy= a.c;
     msg.homography= a.H;
-    msg.pose = homography_to_pose(a.H, -100.,100.,320.,240.)
+    msg.pose = homography_to_pose(a.H, -50.,50.,320.,240.)
     return msg
 end
 
@@ -31,8 +31,8 @@ end
 
 # This colour mapping may be wrong for the PS3eye.
 ycrcb = Video4Linux.YUYV(640,480)
-vidchan = Channel((c::Channel) -> videoproducer(c, ycrcb, devicename = "/dev/video2",
-                                         iomethod = Video4Linux.IO_METHOD_MMAP, N=100 ))
+vidchan = Channel((c::Channel) -> videoproducer(c, ycrcb, devicename = "/dev/video1",
+                                         iomethod = Video4Linux.IO_METHOD_MMAP, N=1000 ))
 
 ##
 #capture one frame to create im1 and canvas needed to diplay
--- examples/apriltagserver/tag_visualizer.jl
+++ examples/apriltagserver/tag_visualizer.jl
@@ -8,31 +8,36 @@ using AprilTags
 vis = Visualizer()
 open(vis)
 
+setobject!(vis["home"], Triad(1.0))
+
 detector = AprilTagDetector()
 # setobject!(vis["tag01"], Triad(0.2))
 # settransform!(vis, Translation(-0.5, -0.5, 0))
 
-function tagPicture(vis::Visualizer,msg::apriltag_t)
+function tagPicture(vis::Visualizer,msg::apriltag_t, drawdict)
     # m=homography_to_pose(Array{Float64,2}(msg.homography),300,300,320,240)
     m = msg.pose
-    setobject!(vis["tag$(msg.id)"], Triad(0.2))
-    wTc=LinearMap(AxisAngle(pi/2,0,1,0))
+    tkey = "tag$(msg.id)"
+    if !haskey(drawdict, tkey)
+        setobject!(vis[tkey], Triad(0.2))
+        drawdict[tkey] = 1
+    end
+    wTc=LinearMap(RotX(pi/2))
     cTt=Translation(m[1:3,4]...) ∘ LinearMap(Quat(m[1:3,1:3]))
-    settransform!(vis, wTc ∘ cTt)
-    # settransform!(vis, Rotations(m[1:3,1:3]...))
+    settransform!(vis[tkey], wTc ∘ cTt)
+    #settransform!(vis[tkey], cTt)
+    nothing
 end
 
-function typed_callback(channel::String, msg::apriltag_t)
-
-    @show channel
-    @show msg
-    tagPicture(vis,msg)
+function typed_callback(channel::String, msg::apriltag_t, drawdict)
+    @show keys(drawdict)
+    tagPicture(vis,msg, drawdict)
 end
 
-
+drawdict = Dict{String, Int}()
 
 lcm=LCM()
-subscribe(lcm, "TEST_CHANNEL", typed_callback, apriltag_t)
+subscribe(lcm, "TEST_CHANNEL", (m,c)->typed_callback(m,c,drawdict), apriltag_t)
 
 
 while true
