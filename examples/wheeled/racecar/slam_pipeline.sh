a83b04d8f0d0bcbee6208f23041d5b405208286a
kurransingh <singh.ku@husky.neu.edu>
2018-11-07 13:47:30 -0500

    filled in bash script to run solves with a variety of parameters

--- examples/wheeled/racecar/slam_pipeline.sh
+++ examples/wheeled/racecar/slam_pipeline.sh
@@ -2,8 +2,18 @@
 
 JULIACMD=julia101
 
-slam_labrun5() {
-  JULIACMD apriltag_and_zed_slam.jl $*
+slam_labruns() {
+  for runNum in {1..8}
+  do
+    for fx in {330..350..4}
+    do
+      for fy in {330..350..4}
+      do
+        for cx in {315..340..5}
+        do
+          for cy in {185..220..5}
+          do
+            JULIACMD apriltag_and_zed_slam.jl --folder_name "labrun$runNum" --cx cx --cy cy --fx fx --fy fy
 }
 
 
@@ -13,4 +23,4 @@ visualize_results() {
 }
 
 
-#
+slam_labruns()
