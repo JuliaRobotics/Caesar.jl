2ccac25e919b0c3ffd652278398b8db47a704a01
dehann <dehann.fourie@gmail.com>
2018-11-11 00:15:35 -0500

    updates

--- examples/wheeled/racecar/apriltag_and_zed_slam.jl
+++ examples/wheeled/racecar/apriltag_and_zed_slam.jl
@@ -4,31 +4,10 @@
 include("parsecommands.jl")
 
 
-# setup configuration
-using YAML
-
-
-# Figure export folder
-global currdirtime = now()
-if parsed_args["previous"] != ""
-  # currdirtime = "2018-10-28T23:17:30.067"
-  # currdirtime = "2018-11-03T22:48:51.924"
-  # currdirtime = "2018-11-07T01:36:52.274"
-  currdirtime = parsed_args["previous"]
-end
-
-resultsparentdir = joinpath(datadir, "results")
-resultsdir = joinpath(resultsparentdir, "$(currdirtime)")
-
-if parsed_args["previous"] == ""
-  # When running fresh from new data
-  include("createResultsDir.jl")
-end
-
 ## Load all required packages
 using Distributed
 
-check_procs(4) # make sure there are 4 processes waiting before loading packages
+# check_procs(4) # make sure there are 4 processes waiting before loading packages
 
 using Dates, Statistics
 @everywhere using Caesar
@@ -53,19 +32,27 @@ using YAML
 
 include(joinpath(dirname(@__FILE__),"configParameters.jl") )
 
+
 # Figure export folder
-currdirtime = now()
-# currdirtime = "2018-10-28T23:17:30.067"
-# currdirtime = "2018-11-03T22:48:51.924"
-# currdirtime = "2018-11-07T01:36:52.274"
-# currdirtime = "2018-11-08T21:56:16.991"
+global currdirtime = now()
+if parsed_args["previous"] != ""
+    # currdirtime = "2018-10-28T23:17:30.067"
+    # currdirtime = "2018-11-03T22:48:51.924"
+    # currdirtime = "2018-11-07T01:36:52.274"
+    # currdirtime = "2018-11-08T21:56:16.991"
+  # currdirtime = "2018-10-28T23:17:30.067"
+  # currdirtime = "2018-11-03T22:48:51.924"
+  # currdirtime = "2018-11-07T01:36:52.274"
+  currdirtime = parsed_args["previous"]
+end
+
 resultsparentdir = joinpath(datadir, "results")
 resultsdir = joinpath(resultsparentdir, "$(currdirtime)")
 
-
-# When running fresh from new data
-include(joinpath(dirname(@__FILE__),"createResultsDirs.jl") )
-
+if parsed_args["previous"] == ""
+  # When running fresh from new data
+  include("createResultsDir.jl")
+end
 
 
 # Utils required for this processing script
@@ -87,7 +74,7 @@ end
 
 
 
-fg = main(resultsdir, camidxs, tag_bag, )
+fg = main(resultsdir, camidxs, tag_bag, jldfile=parsed_args["jldfile"])
 
 0
 
