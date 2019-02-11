e5ba77b28fbc43950103ee816e6202b2074ab53a
dehann <dehann.fourie@gmail.com>
2019-02-11 12:06:34 -0500

    fixing travsi

--- docs/make.jl
+++ docs/make.jl
@@ -2,7 +2,6 @@ using Documenter, Caesar
 import IncrementalInference: fmcmc!, localProduct, productpartials!, prodmultiplefullpartials, prodmultipleonefullpartials, setfreeze!
 import IncrementalInference: cliqGibbs, downMsgPassingRecursive, packFromLocalPotentials!, treeProductDwn, updateFGBT!, upGibbsCliqueDensity
 
-using Cairo
 using KernelDensityEstimatePlotting
 using RoMEPlotting
 using Arena
