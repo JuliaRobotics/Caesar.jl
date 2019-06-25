33ea12ad409a7e938d21ab759d1eda3f8da95866
dehann <dehann.fourie@gmail.com>
2019-06-25 15:34:34 -0400

    fixing fwd bck

--- test/beamforming/testFwdBackConv.jl
+++ test/beamforming/testFwdBackConv.jl
@@ -6,7 +6,7 @@ using Colors, Gadfly
 using RoMEPlotting, KernelDensityEstimatePlotting
 
 #posFile = joinpath(dirname(pathof(Caesar)),"..","test","testdata","test_array_positions.csv");
-posFile = joinpath("/home",ENV["USER"],"data","sas","test_array_positions.csv");
+posFile = joinpath(ENV["HOME"],"data","sas","test_array_positions.csv");
 posData = readdlm(posFile,',',Float64,'\n')
 
 
@@ -16,12 +16,14 @@ posData[:,2] .-= 40.0
 # gps covariance
 rtkCov = Matrix(Diagonal([0.1;0.1].^2))
 
+
+## build the factor graph
 N = 100
 fg = initfg()
 
 #landmark
 addVariable!(fg, :l1, Point2)
-setVal!(fg, :l1, zeros(2,N))
+# setVal!(fg, :l1, zeros(2,N))
 
 global pp = nothing
 for i in 1:5
@@ -36,20 +38,25 @@ for i in 1:5
 end
 
 #dataFile = joinpath(dirname(pathof(Caesar)),"..","test","testdata","test_array_waveforms.csv");
-dataFile = joinpath("/home",ENV["USER"],"data","sas","test_array_waveforms.csv");
+dataFile = joinpath(ENV["HOME"],"data","sas","test_array_waveforms.csv");
 
 csvWaveData = readdlm(dataFile,',',Float64,'\n')
 csvWaveData = csvWaveData[:,1:5]
 #
-cfgFile = joinpath("/home",ENV["USER"],"data","sas","SAS2D.yaml");
+cfgFile = joinpath(ENV["HOME"],"data","sas","SAS2D.yaml");
 cfgd=loadConfigFile(cfgFile)
-chirpFile = joinpath("/home",ENV["USER"],"data","sas","chirp250.txt");
+chirpFile = joinpath(ENV["HOME"],"data","sas","chirp250.txt");
 sas2d = prepareSAS2DFactor(5, csvWaveData, rangemodel=:Correlator, cfgd=cfgd, chirpFile=chirpFile)
 
 
 
 #
-addFactor!(fg, [:l1;:x1;:x2;:x3;:x4;:x5], sas2d, autoinit=false) #, threadmodel=SingleThreaded ) #;:x6;:x7;:x8;:x9
+addFactor!(fg, [:l1;:x1;:x2;:x3;:x4;:x5], sas2d, autoinit=false)
+#, threadmodel=SingleThreaded )
+
+
+writeGraphPdf(fg, engine="fdp")
+
 
 # sas2d = getfnctype(getVert(fg, :l1x1x2x3x4x5f1, nt=:fnc)) # deepcopy somewhere in IIF
 
@@ -57,10 +64,16 @@ addFactor!(fg, [:l1;:x1;:x2;:x3;:x4;:x5], sas2d, autoinit=false) #, threadmodel=
 # ls(fg, :l1)
 # getVal(fg, :l1)
 
+getSolverParams(fg).drawtree = true
+getSolverParams(fg).showtree = true
+
+tree, smt, hist = solveTree!(fg)
+
+drawTree(tree, show=true)
 
 
 # forward SAS case
-for ti in 1:Threads.nthreads() reset!(sas2d.threadreuse[ti].dbg) end
+# for ti in 1:Threads.nthreads() reset!(sas2d.threadreuse[ti].dbg) end
 
 @time predL1 = IncrementalInference.approxConv(fg, ls(fg, :l1)[1], :l1, N=N)
 # setVal!(fg, :l1, zeros(2,N))
@@ -72,117 +85,20 @@ X1 = getVal(fg, :x1)
 pl = Gadfly.plot(
   Gadfly.layer(x=predL1[1,:][-pllim .< predL1[1,:] .< pllim],y=predL1[2,:][-pllim .< predL1[2,:] .< pllim],   Gadfly.Geom.hexbin(xbincount=100, ybincount=100)),
   Gadfly.layer(x=X1[1,:],y=X1[2,:], Gadfly.Geom.hexbin(xbincount=100, ybincount=100))
-)
+) #; # for skipping Juno display
 
 
+pl |> PDF("/tmp/test.pdf"); @async run(`evince /tmp/test.pdf`)
+pl |> SVG("/tmp/test.svg", 35cm, 25cm); @async run(`firefox /tmp/test.svg`)
 
-# backwards case
 
 
-
-stuff = IIF.localProduct(fg, :x5)
-stuff[2]
-
-plotKDE([getVertKDE(fg, :x5);stuff[2]], dims=[1;2], levels=3, c=["red";"green";["blue" for i in 1:10]])
-pts = KDE.getPoints(stuff[2][1])
-plot(x=pts[1,:],y=pts[2,:], Geom.hexbin)
-
-
-
-
-
-
-
-
-setVal!(fg, :l1, predL1)
-
-
-L1 = getVal(fg, :l1)
-pl = plotKDE(kde!(L1))
-# pl.coord = Gadfly.Coord.Cartesian(xmin=-200, xmax=100, ymin=-100, ymax=200);
-pl
-
-
-
-
-
-# Look at sas2d.dbg
-# @show length(sas2d.dbg)
-plot(x=sas2d.dbg.azi_smpls, Geom.histogram)
-
-
-
-n=200; pl = plot(x=linspace(0,2pi,length(sas2d.dbg.beams[n])), y=sas2d.dbg.beams[n], Geom.path())
-
-
-n=200; pl = plot(x=linspace(0,2pi,length(sas2d.dbg.beams_cut[n])), y=sas2d.dbg.beams_cut[n], Geom.path())
-
-
-
-
-
-avg_beam_cut = zeros(length(sas2d.dbg.beams_cut[1]))
-for i in 1:N
-  avg_beam_cut[:] = avg_beam_cut + sas2d.dbg.beams_cut[i]
-end
-avg_beam_cut ./= sum(avg_beam_cut)
-
-
-
-
-avg_beam = zeros(length(sas2d.dbg.beams[1]))
-for i in 1:N
-  avg_beam[:] = avg_beam + sas2d.dbg.beams[i]
-end
-avg_beam ./= sum(avg_beam)
-
-
-
-
-
-
-circ_avg_beam = zeros(2,length(avg_beam))
-
-count = 0
-for th in linspace(0,2pi,length(avg_beam))
-  count += 1
-  circ_avg_beam[:,count] = 7500*R(th)*[avg_beam[count];0.0]
-end
-
-
-
-
-
-pl = plot(x=linspace(0,2pi,length(avg_beam)), y=avg_beam, Geom.path(), Coord.Cartesian(ymin=0))
-pl = plot(x=linspace(0,2pi,length(avg_beam_cut)), y=avg_beam_cut, Geom.path(), Coord.Cartesian(ymin=0))
-
-
-# not the median cut beam
-plPica = plot(x=circ_avg_beam[1,:], y=circ_avg_beam[2,:], Geom.path(), Theme(default_color=colorant"magenta", line_width=2pt))
-
-
-plPica.layers = union(plPica.layers, pl.layers)
-
-
-
-
-plPica
-
-0
-
-
-
-
-
-
-
-# and now the backwards solve
-
+## backwards case
 
 # backwards leave one out case
 setVal!(fg, :l1, 1.0*randn(2,N))
-setVal!(fg, :x2, rand(MvNormal(posData[2,1:2], 1.0*eye(2)),N))
-for ti in 1:Threads.nthreads() reset!(sas2d.threadreuse[ti].dbg) end
+setVal!(fg, :x2, rand(MvNormal(posData[2,1:2], 1.0*Matrix{Float64}(Diagonal([1.0;1.0]))),N))
+# for ti in 1:Threads.nthreads() reset!(sas2d.threadreuse[ti].dbg) end
 
 @time predX2 = IncrementalInference.approxConv(fg, ls(fg, :l1)[1], :x2, N=N)
 
@@ -195,14 +111,14 @@ layer(x=posData[1:5,1],y=posData[1:5,2], Geom.point)
 )
 
 pl.coord = Coord.Cartesian(xmin=0,xmax=20,ymin=-50,ymax=-35)
-pl
+pl |> SVG("/tmp/test.svg", 35cm, 25cm)
 
 
 
 pl_wait = plotKDE(kde!(predX2))
 pl.layers = [pl.layers; pl_wait.layers]
 
-pl
+pl |> SVG("/tmp/test.svg", 35cm, 25cm)
 
 0
 
@@ -217,7 +133,7 @@ function main()
 
 
     sasframe = 64
-    logdir = "/media/data1/data/kayaks/20_parsed/array_positions$(sasframe).csv"  # 85
+    logdir = joinpath(ENV["HOME"],"data", "kayaks", "20_parsed", "array_positions$(sasframe).csv")  # 85
     posData = readdlm(logdir,',',Float64,'\n')
 
     posData[:,1] += 13.0
