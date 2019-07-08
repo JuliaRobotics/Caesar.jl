8a1ac4dbc9fd10ad7d3aa305aad0f3504eda6581
dehann <dehann.fourie@gmail.com>
2019-07-08 10:57:08 -0400

    wip on dbg, upstream changes to iif, dfg

--- examples/marine/asv/kayaks/rangeOnlySLAM.jl
+++ examples/marine/asv/kayaks/rangeOnlySLAM.jl
@@ -1,9 +1,41 @@
 
-
 using Caesar, DelimitedFiles
 using KernelDensityEstimatePlotting
 using Gadfly, Cairo, Fontconfig
 
+# include(joinpath(@__DIR__,"slamUtils.jl"))
+#
+# rangesin = zeros(1000,320);
+# for files in collect(60:20:360)
+#     rangeFile = joinpath(ENV["HOME"],"data", "kayaks","rangesonly_6_20","range$(files).txt");
+#     rangesin[:,files-60+1:files-60+20] = readdlm(rangeFile,',',Float64,'\n')
+# end
+# datadir = joinpath(ENV["HOME"],"data", "kayaks","20_gps_pos")
+#
+# window = 70:20:130;
+#
+# posDataAll = zeros(window[end]-window[1]+1,2);
+# for i in window[1]:window[end]
+#     navfile = datadir*"/nav$i.csv"
+#     posDataAll[i-window[1]+1,:] = readdlm(navfile,',',Float64,'\n')
+# end
+#
+# dposData = deepcopy(posDataAll)
+# cumulativeDrift!(dposData,[0.0;0],[0.2,0.2])
+#
+# #Gadfly.plot(layer(x=posDataAll[:,1],y=posDataAll[:,2], Geom.path, Theme(default_color=colorant"green")), layer(x=dposData[:,1],y=dposData[:,2],Geom.path))
+#
+# fg = initfg();
+# beacon = :l1
+# addVariable!(fg, beacon, Point2 )
+#
+# fullwindow = 1:window[end]-window[1]+1
+# fullwindowt = 1:window[end]-window[1]
+# for i in fullwindow
+#     sym = Symbol("x$i")
+#     addVariable!(fg, sym, Point2)
+# end
+
 include(joinpath(@__DIR__,"slamUtils.jl"))
 
 rangesin = zeros(1000,320);
@@ -14,39 +46,7 @@ end
 datadir = joinpath(ENV["HOME"],"data", "kayaks","20_gps_pos")
 
 window = 70:20:130;
-
-posDataAll = zeros(window[end]-window[1]+1,2);
-for i in window[1]:window[end]
-    navfile = datadir*"/nav$i.csv"
-    posDataAll[i-window[1]+1,:] = readdlm(navfile,',',Float64,'\n')
-end
-
-dposData = deepcopy(posDataAll)
-cumulativeDrift!(dposData,[0.0;0],[0.2,0.2])
-
-#Gadfly.plot(layer(x=posDataAll[:,1],y=posDataAll[:,2], Geom.path, Theme(default_color=colorant"green")), layer(x=dposData[:,1],y=dposData[:,2],Geom.path))
-
-fg = initfg();
-beacon = :l1
-addVariable!(fg, beacon, Point2 )
-
-fullwindow = 1:window[end]-window[1]+1
-fullwindowt = 1:window[end]-window[1]
-for i in fullwindow
-    sym = Symbol("x$i")
-    addVariable!(fg, sym, Point2)
-end
-
-include(joinpath(@__DIR__,"slamUtils.jl"))
-
-rangesin = zeros(1000,320);
-for files in collect(60:20:360)
-    rangeFile = joinpath(ENV["HOME"],"data", "kayaks","rangesonly_6_20","range$(files).txt");
-    rangesin[:,files-60+1:files-60+20] = readdlm(rangeFile,',',Float64,'\n')
-end
-datadir = joinpath(ENV["HOME"],"data", "kayaks","20_gps_pos")
-
-window = 70:20:130;
+window = 70:20:100;
 
 posDataAll = zeros(window[end]-window[1]+1,2);
 for i in window[1]:window[end]
@@ -147,8 +147,11 @@ end
 getSolverParams(fg).drawtree = true
 getSolverParams(fg).showtree = true
 getSolverParams(fg).async = true
+getSolverParams(fg).downsolve = false
+
 
 tree, smt, hist = solveTree!(fg, recordcliqs=ls(fg)) # [:l1;]
+
 
 L1v = getVariable(fg, beacon)
 L1 = getVal(L1v)
@@ -174,3 +177,68 @@ igt = [17.0499;1.7832];
 push!(plk,layer(x=[igt[1];],y=[igt[2];], label=String["Beacon Ground Truth";],Geom.point,Geom.label(hide_overlaps=false), order=2, Theme(default_color=colorant"red")));
 
 plkplot = Gadfly.plot(plk...); plkplot |> PDF("/tmp/test.pdf")
+@async run(`evince /tmp/test.pdf`)
+
+
+
+
+
+
+
+
+
+## dev work below
+
+getVariableType(fg, :x1)
+
+
+lsfPriors(fg)
+# lsfPriors(fg) = Symbol[:x1f2, :x1f1, :x60f1, :x60f2]
+
+## moved to DistributedFactorGraphs
+# """
+#     $SIGNATURES
+#
+# Return `::Bool` on whether given factor `fc::Symbol` is a prior in factor graph `dfg`.
+# """
+# function isPrior(dfg::G, fc::Symbol)::Bool where G <: AbstractDFG
+#   fco = getFactor(dfg, fc)
+#   getfnctype(fco) isa FunctorSingleton
+# end
+#
+# """
+#     $SIGNATURES
+#
+# Return vector of prior factor symbol labels in factor graph `dfg`.
+# """
+# function lsfPriors(dfg::G)::Vector{Symbol} where G <: AbstractDFG
+#   priors = Symbol[]
+#   fcts = lsf(dfg)
+#   for fc in fcts
+#     if isPrior(dfg, fc)
+#       push!(priors, fc)
+#     end
+#   end
+#   return priors
+# end
+# """
+# $SIGNATURES
+#
+# Return the DFGVariable softtype in factor graph `dfg<:AbstractDFG` and label `::Symbol`.
+# """
+# getVariableType(var::DFGVariable) = getSofttype(var)
+# function getVariableType(dfg::G, lbl::Symbol) where G <: AbstractDFG
+#     getVariableType(getVariable(dfg, lbl))
+# end
+
+getFactorType(fg, :x1f1)
+
+function lsfTypes(dfg::G) where G <: AbstractDFG
+  alltypes = String[]
+  for fc in lsf(dfg)
+    union!(alltypes, getFactorType(dfg, fc)
+  end
+end
+
+
+#
