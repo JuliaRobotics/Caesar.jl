00121348f232187b2df90ff5c75b37143e05e448
Mei Yi Cheung <mc2922@gmail.com>
2019-07-31 21:20:58 -0700

    combine scripts

--- examples/marine/asv/kayaks/dynsas_ljscript.jl
+++ examples/marine/asv/kayaks/dynsas_ljscript.jl
@@ -9,26 +9,29 @@ using DocStringExtensions
 using DelimitedFiles, JLD
 
 include(joinpath(@__DIR__,"slamUtils.jl"))
+include(joinpath(@__DIR__,"plotSASUtils.jl"))
+include(joinpath(@__DIR__,"expDataUtils.jl"))
 
-function main(savedir::String, datastart::Int, dataend::Int, fgap::Int, gps_gap::Int; saswindow::Int=8)
+function main(expID::String, datastart::Int, dataend::Int, fgap::Int, gps_gap::Int; saswindow::Int=8, trialID::Int=1)
     ## Default parameters
     println("Start Script\n")
-    symbolstart = 1;
-    saswindow = 8;
-    totalposes = dataend-datastart;
+
     sas_gap = saswindow+fgap;
     tstart = 1_000_000;
     element = 2
-    savedirheader = savedir * "/drift_fgap$(fgap)_gpsgap$(gps_gap)_swind$(saswindow)";
+
+    totalposes = dataend-datastart;
+    datawindow = collect(datastart:dataend);
+    allpaths = getPaths(expID,"dynsas", currloc = "lj", trialID = trialID);
+    scriptHeader = joinpath(allpaths[3],expID*"_fgap$(fgap)_gpsgap$(gps_gap)_swind$(saswindow)_f$(datastart)t$(dataend)_")
 
     poses = Dict{Int,Array}();
     nav = Dict{Int,Array}();
     errorinds = [];
     allsasframes = Dict{Int,Array}();
-    saveBF = Dict();
 
-    allframes = collect(datastart:dataend);
-    posData = importdata_nav(allframes);
+    igt = loadGT(datawindow,expID,allpaths);
+    posData = loadNav(datawindow,allpaths)
     dposData = deepcopy(posData)
     i=1
     while i+gps_gap < size(dposData,1)
@@ -39,9 +42,8 @@ function main(savedir::String, datastart::Int, dataend::Int, fgap::Int, gps_gap:
         end
     end
 
-    # posPl = Gadfly.plot(layer(x=posData[:,1],y=posData[:,2], Geom.path, Theme(default_color=colorant"green")), layer(x=dposData[:,1],y=dposData[:,2],Geom.path)); posPl |> PDF(savedirheader*"_posGT.pdf")
+    # posPl = Gadfly.plot(layer(x=posData[:,1],y=posData[:,2], Geom.path, Theme(default_color=colorant"green")), layer(x=dposData[:,1],y=dposData[:,2],Geom.path)); posPl |> PDF(scriptHeader*"posGT.pdf")
 
-    dataDir = joinpath("/media","data1","data","kayaks","08_10_parsed")
     chirpFile = joinpath("/media","data1","data","kayaks","chirp250.txt");
     cfgFile = joinpath("/media","data1","data","kayaks","SAS2D.yaml")
     cfgd=loadConfigFile(cfgFile)
@@ -49,7 +51,7 @@ function main(savedir::String, datastart::Int, dataend::Int, fgap::Int, gps_gap:
     fg = initfg();
     beacon = :l1
     addVariable!(fg, beacon, Point2 )
-    tree=emptyBayesTree();
+    tree = emptyBayesTree();
 
     pose_counter = 1
     sas_counter = 1
@@ -81,7 +83,7 @@ function main(savedir::String, datastart::Int, dataend::Int, fgap::Int, gps_gap:
            xdotf = dposData[pose_counter+1,1] - dposData[pose_counter,1];
            ydotf = dposData[pose_counter+1,2] - dposData[pose_counter,2];
            dpμ = [xdotp;ydotp;xdotf-xdotp;xdotf-xdotp];
-           dpσ = Matrix(Diagonal([0.5;0.5;0.2;0.2].^2))
+           dpσ = Matrix(Diagonal([0.2;0.2;0.2;0.2].^2))
            vp = VelPoint2VelPoint2(MvNormal(dpμ,dpσ))
            addFactor!(fg, [Symbol("x$(pose_counter-1)");current_symbol], vp, autoinit=false)
            # manualinit!(fg,current_symbol,kde!(rand(MvNormal(dpμ,dpσ),100)))
@@ -104,7 +106,7 @@ function main(savedir::String, datastart::Int, dataend::Int, fgap::Int, gps_gap:
                sas2d = prepareSAS2DFactor(saswindow, waveformData, rangemodel=:Correlator,cfgd=cfgd, chirpFile=chirpFile)
                addFactor!(fg, [beacon;poses[sas_counter]], sas2d, autoinit=false)
 
-               getSolverParams(fg).drawtree = true
+               getSolverParams(fg).drawtree = false
                getSolverParams(fg).showtree = false
 
                if sas_counter > 1
@@ -113,44 +115,34 @@ function main(savedir::String, datastart::Int, dataend::Int, fgap::Int, gps_gap:
                    tree, smt, hist = solveTree!(fg)
                end
 
-               plk= [];
+               writeGraphPdf(fg,viewerapp="", engine="neato", filepath=scriptHeader*"fg.pdf")
+               drawTree(tree, filepath=scriptHeader*"bt.pdf")
 
-               igt = [17.0499;1.7832];
-               push!(plk,layer(x=[igt[1];],y=[igt[2];], label=String["Beacon";],Geom.point,Geom.label(hide_overlaps=false), order=2, Theme(default_color=colorant"red")));
+               plotSASDefault(fg,expID, posData,igt,datadir=allpaths[1],savedir=scriptHeader*"$sas_counter.pdf")
 
-               for i in 1:sas_counter
-                 for mysym in poses[i]
-                   X1 = getKDEMean(getVertKDE(fg,mysym))
-                   push!(plk, layer(x=[X1[1];],y=[X1[2];], Geom.point), Theme(default_color=colorant"orange",point_size = 1.5pt,highlight_width = 0pt))
+               if expID == "dock"
+                   l1fit = getKDEMean(getVertKDE(fg,:l1))
+                   meanerror = sqrt((igt[1]-l1fit[1])^2+(igt[2]-l1fit[2])^2)
+
+                   l1max = getKDEMax(getVertKDE(fg,:l1))
+                   maxerror = sqrt((igt[1]-l1max[1])^2+(igt[2]-l1max[2])^2)
+
+                   mykde = getVertKDE(fg,:l1)
+                   mynorm = kde!(rand(fit(MvNormal,L1),200))
+                   kld = min(abs(KernelDensityEstimate.kld(mynorm,mykde)),abs(KernelDensityEstimate.kld(mykde,mynorm)))
+               elseif expID == "drift"
+                   l1fit = getKDEMean(getVertKDE(fg,:l1))
+                   meanerror = sqrt((mean(igt[:,1])-l1fit[1])^2+(mean(igt[:,2])-l1fit[2])^2)
+
+                   l1max = getKDEMax(getVertKDE(fg,:l1))
+                   maxerror = sqrt((mean(igt[:,1])-l1max[1])^2+(mean(igt[:,2])-l1max[2])^2)
+
+                   mykde = getVertKDE(fg,:l1)
+                   mynorm = kde!(rand(fit(MvNormal,L1),200))
+                   kld = min(abs(KernelDensityEstimate.kld(mynorm,mykde)),abs(KernelDensityEstimate.kld(mykde,mynorm)))
                end
-              end
 
-              for i in 1:pose_counter
-                    X1 = getKDEMean(getVertKDE(fg,Symbol("x$i")))
-                    push!(plk, layer(x=[X1[1];],y=[X1[2];], Geom.point), Theme(point_size = 1.5pt,highlight_width = 0pt))
-               end
-
-               push!(plk,layer(x=posData[:,1],y=posData[:,2], Geom.path, Theme(default_color=colorant"green")))
-
-               L1 = getVal(getVariable(fg, beacon))
-               K1 = plotKDEContour(getVertKDE(fg,:l1),xlbl="X (m)", ylbl="Y (m)",levels=5,layers=true);
-               push!(plk,K1...)
-               push!(plk,Gadfly.Theme(key_position = :none));
-               push!(plk, Coord.cartesian(xmin=-40, xmax=140, ymin=-150, ymax=75,fixed=true))
-
-               plkplot = Gadfly.plot(plk...); plkplot |> PDF(savedirheader*"_sasframe$sas_counter.pdf");
-
-               l1fit = getKDEMean(getVertKDE(fg,:l1))
-               meanerror = sqrt((igt[1]-l1fit[1])^2+(igt[2]-l1fit[2])^2)
-
-               l1max = getKDEMax(getVertKDE(fg,:l1))
-               maxerror = sqrt((igt[1]-l1max[1])^2+(igt[2]-l1max[2])^2)
-
-               mykde = getVertKDE(fg,:l1)
-               mynorm = kde!(rand(fit(MvNormal,L1),200))
-               kld = min(abs(KernelDensityEstimate.kld(mynorm,mykde)),abs(KernelDensityEstimate.kld(mykde,mynorm)))
-
-               jldname2 = savedirheader * "_solve_$(sas_counter).jld"
+               jldname2 = scriptHeader * "solve_$(sas_counter).jld"
                JLD.save(jldname2,"beacon",L1,"posData",posData,"dposData", dposData,"gps_gap", gps_gap, "poses",poses,"sasframes", allsasframes, "l1fit",l1fit, "meanerror",meanerror,"l1max",l1max,"maxerror",maxerror,"kld",kld)
 
                sas_counter +=1
