1ff454fc7cf919b4d9054344e35c600c03008cc0
Mei Yi Cheung <mc2922@gmail.com>
2019-08-04 12:03:34 -0700

    savedfg + pose errorrs

--- examples/marine/asv/kayaks/dynsas_ljscript.jl
+++ examples/marine/asv/kayaks/dynsas_ljscript.jl
@@ -158,12 +158,21 @@ function main(expID::String, datastart::Int, dataend::Int, fgap::Int, gps_gap::I
                    kld = min(abs(KernelDensityEstimate.kld(mynorm,mykde)),abs(KernelDensityEstimate.kld(mykde,mynorm)))
                end
 
+               ev = 0;
+               for tmpi = 1:pose_counter
+                   rv = getVal(fg,Symbol("x$tmpi"));
+                   dxt = (rv[1,:].-posData[tmpi,1])^2;
+                   dyt = (rv[2,:].-posData[tmpi,2])^2;
+                   ev += sum(sqrt.(dxt+dyt));
+               end
+               ev = ev./pose_counter;
+
                jldname2 = scriptHeader * "solve_$(sas_counter).jld"
-               JLD.save(jldname2,"beacon",getVal(fg,:l1),"posData",posData,"dposData", dposData,"gps_gap", gps_gap, "poses",poses,"sasframes", allsasframes, "l1fit",l1fit, "meanerror",meanerror,"l1max",l1max,"maxerror",maxerror,"kld",kld)
+               JLD.save(jldname2,"beacon",getVal(fg,:l1),"posData",posData,"dposData", dposData,"gps_gap", gps_gap, "poses",poses,"sasframes", allsasframes, "l1fit",l1fit, "meanerror",meanerror,"l1max",l1max,"maxerror",maxerror,"kld",kld,"ev",ev)
 
-               # saveDFG(fg,scriptHeader * "fg$(sas_counter)")
+               saveDFG(fg,scriptHeader * "fg$(sas_counter)")
 
-               writedlm(scriptHeader*"stats.txt", [meanerror maxerror kld], ",")
+               writedlm(scriptHeader*"stats.txt", [meanerror maxerror kld ev], ",")
 
                sas_counter +=1
                sas_gap_counter = 0
