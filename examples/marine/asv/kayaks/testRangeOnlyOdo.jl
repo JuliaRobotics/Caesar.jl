10946c264ca5ab18e3f53e8cc29d0021d2f6869c
dehann <dehann.fourie@gmail.com>
2019-09-02 23:50:20 -0400

    troubleshooting odd odo

--- examples/marine/asv/kayaks/testRangeOnlyOdo.jl
+++ examples/marine/asv/kayaks/testRangeOnlyOdo.jl
@@ -1,9 +1,11 @@
+using Revise
 
 using Caesar
 using KernelDensityEstimatePlotting
 using KernelDensityEstimate, IncrementalInference
 using DocStringExtensions, DelimitedFiles
 using Gadfly, Cairo, Fontconfig
+Gadfly.set_default_plot_size(35cm,25cm)
 
 include(joinpath(@__DIR__,"slamUtils.jl"))
 
@@ -64,13 +66,17 @@ for i in rangewindow
     addFactor!(fg, [sym;beacon], ppR, autoinit=false)
 end
 
-writeGraphPdf(fg,viewerapp="", engine="neato", filepath="/tmp/fg.pdf")
+# writeGraphPdf(fg,viewerapp="", engine="neato", filepath="/tmp/fg.pdf")
 getSolverParams(fg).drawtree = true
 getSolverParams(fg).showtree = false
+getSolverParams(fg).dbg = true
+
 
 ensureAllInitialized!(fg)
-tree, smt, hist = solveTree!(fg,maxparallel=200)
-drawTree(tree, filepath="/tmp/bt.pdf")
+tree, smt, hist = solveTree!(fg,maxparallel=200, recordcliqs=[:x19;:x169;:x166;:x134;:x171])
+
+fetchCliqTaskHistoryAll!(smt, hist)
+drawTree(tree, show=true)
 
 #Plotting
 
@@ -96,7 +102,7 @@ push!(plk,layer(x=dposData[:,1],y=dposData[:,2], Geom.path, Theme(default_color=
 push!(plk, Coord.cartesian(xmin=95, xmax=105, ymin=-81, ymax=-70,fixed=true)) #crop plot to last points
 plkplot = Gadfly.plot(plk...); plkplot |> PDF("/tmp/test.pdf");
 
-#
+
 # igt = [17.0499;1.7832];
 #
 # push!(plk,layer(x=[igt[1];],y=[igt[2];], label=String["Beacon Ground Truth";],Geom.point,Geom.label(hide_overlaps=false), order=2, Theme(default_color=colorant"red",highlight_width = 0pt)));
@@ -120,3 +126,83 @@ plkplot = Gadfly.plot(plk...); plkplot |> PDF("/tmp/test.pdf");
 bss = AliasingScalarSampler(mfin[:,1,4],exp.(mfin[:,2,4]),SNRfloor=0.8)
 out = rand(bss,1000)
 Gadfly.plot(x=out,Geom.histogram,Coord.cartesian(xmin=0, xmax=100, ymin=0, ymax=400,fixed=true))
+
+
+
+
+## dehann dev
+
+using RoMEPlotting
+
+# tree = wipeBuildNewTree!(fg, maxparallel=200)
+# spyCliqMat(tree, :x169)
+
+drawTree(tree, show=true)
+
+
+plotTreeProductDown(fg, tree, :x169, :x169)
+plotTreeProductDown(fg, tree, :x169, :x170)
+
+
+#
+getCliq(tree, :x169)
+
+
+# fldr = "2019-09-01T12:25:09.297"
+fldr = "2019-09-02T12:29:03.747"
+# fg31_au = loadDFG("/tmp/caesar/"*fldr*"/cliqSubFgs/cliq31/fg_afterupsolve", Main)
+# fg31_bu = loadDFG("/tmp/caesar/"*fldr*"/cliqSubFgs/cliq31/fg_beforeupsolve", Main)
+
+fg31bd = initfg()
+loadDFG("/tmp/caesar/"*fldr*"/cliqSubFgs/cliq31/fg_beforedownsolve", Main, fg31bd)
+# fg31_bd =
+
+
+
+writeGraphPdf(fg31bd, show=true)
+
+
+plotLocalProduct(fg31bd, :x169)
+plotLocalProduct(fg31bd, :x170)
+
+
+printCliqHistorySummary(hist, tree, :x169)
+
+
+
+sfg31bd = buildSubgraphFromLabels(fg31bd, [:x167;:x168;:x169;:x170])
+
+
+writeGraphPdf(sfg31bd, show=true)
+
+
+tree31s, smt31s, hist31s = solveTree!(sfg31bd)
+
+drawTree(tree31s, show=true, imgs=true)
+
+
+plotKDE(sfg31bd, ls(sfg31bd), levels=1)
+
+plotLocalProduct(sfg31bd, :x169)
+plotLocalProduct(sfg31bd, :x168)
+
+
+
+# need a new factor plot debug function
+#
+
+
+
+
+plotVariableGivenFactor(sfg31bd,:x168x169f1,:x169)
+
+pts = approxConv(sfg31bd,:x168x169f1, :x169)
+lie = ls(sfg31bd, :x168x169f1)
+setdiff!(lie, [:x169])
+
+
+fct = getFactor(sfg31bd, :x168x169f1)
+@show getFactorType(fct)
+getData(fct).fncargvID
+
+#
