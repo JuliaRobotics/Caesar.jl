b551777972714f7f75e0bdda095ab9a5b6a87fad
dehann <dehann.fourie@gmail.com>
2019-12-30 01:13:34 -0500

    fix docs

--- examples/marine/auv/Sandshark/ExampleCommand.sh
+++ examples/marine/auv/Sandshark/ExampleCommand.sh
@@ -1,2 +1,2 @@
 julia -O3 LCM_SLAM_client.jl --dbg -i 10000 -s 0.2 --kappa_odo 10 --magStdDeg 10.0
-
+julia -O3 LCM_SLAM_client.jl --dbg -i 40000 -s 0.13 --kappa_odo 10.0 --magStdDeg 2.0 --stride_range 3
