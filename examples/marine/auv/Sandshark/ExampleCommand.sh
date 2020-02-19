4ddc1f284af5dcfcc30e2850134fb0cd28cc591e
dehann <dehann.fourie@gmail.com>
2020-02-19 11:20:23 -0500

    update commands

--- examples/marine/auv/Sandshark/ExampleCommand.sh
+++ examples/marine/auv/Sandshark/ExampleCommand.sh
@@ -5,9 +5,9 @@
 
 # sleep 1400; JULIA_NUM_THREADS=10 julia -O3 GenerateResults.jl /tmp/caesar/2020-01-29T01\:13\:04.735/fg_after_x781.tar.gz --plotSeriesBeliefs 0 --skip
 
-# JULIA_NUM_THREADS=4 julia -O3 -p6 LCM_SLAM_client.jl -i 300000 -s 0.1 --kappa_odo 20.0 --magStdDeg 5.0 --stride_range 3 --odoGyroBias --dbg --savePlotting
-#  --dbg --savePlotting --fixedlag 50
+# JULIA_NUM_THREADS=4 julia -O3 -p12 LCM_SLAM_client.jl -i 20000 -s 0.1 --kappa_odo 20.0 --magStdDeg 5.0 --stride_range 3 --odoGyroBias --dbg --savePlotting --fixedlag 100
+
+  --dbg --savePlotting
 #
 # ffmpeg -start_number 21 -i /tmp/caesar/2020-01-27T01\:46\:52.871/lines/fg_x%d.png -c:v libx264 -r 30 -pix_fmt yuv420p ~/Videos/out871.mp4
 # ffmpeg -start_number 21 -i /tmp/caesar/2020-01-29T01\:13\:04.735/lines/fg_x%d.png -c:v h264_nvenc -r 30 -pix_fmt yuv420p ~/Videos/out735.mp4
-
