ce0953f488c053c123da844460181c363a197dbb
dehann <dehann.fourie@gmail.com>
2020-04-16 10:27:24 -0400

    wip

--- examples/learning/hybrid/dev/NNOdoModelDev.jl
+++ examples/learning/hybrid/dev/NNOdoModelDev.jl
@@ -163,44 +163,6 @@ c5_py = pymodel.predict([[pyX]])
 
 
 
-##  Utility functions to take values from tf
-
-
-function buildPyNNModel_01_FromElements(W1::AbstractMatrix{<:Real}=zeros(4,8),
-                                        b1::AbstractVector{<:Real}=zeros(8),
-                                        W2::AbstractMatrix{<:Real}=zeros(8,48),
-                                        b2::AbstractVector{<:Real}=zeros(8),
-                                        W3::AbstractMatrix{<:Real}=zeros(2,8),
-                                        b3::AbstractVector{<:Real}=zeros(2))
-  #
-  # W1 = randn(Float32, 4,8)
-  # b1 = randn(Float32,8)
-  modjl = Chain(
-    x -> (x*W1)' .+ b1 .|> relu,
-    x -> reshape(x', 25,8,1),
-    x -> maxpool(x, PoolDims(x, 4)),
-    # x -> reshape(x[:,:,1]',1,:),
-    x -> reshape(x[:,:,1]',:),
-    Dense(48,8,relu),
-    Dense(8,2)
-  )
-
-  modjl[5].W .= W2
-  modjl[5].b .= b2
-
-  modjl[6].W .= W3
-  modjl[6].b .= b3
-
-  return modjl
-end
-
-# As loaded from tensorflow get_weights
-# Super specialized function
-function buildPyNNModel_01_FromWeights(pywe)
-  buildPyNNModel_01_FromElements(pywe[1], pywe[2][:], pywe[3]', pywe[4][:], pywe[5]', pywe[6][:])
-end
-
-
 ## test with lots of variability
 
 
