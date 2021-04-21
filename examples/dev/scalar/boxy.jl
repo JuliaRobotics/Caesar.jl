aa45767dd38929a5257fecc4755e1c39482de5b9
dehann <dehann.fourie@gmail.com>
2021-04-21 18:59:50 -0400

    boxy matchLeg NORTH works (not SOUTH yet)
    Co-authored-by: Pedro Vaz Teixeira <pvazteixeira@gmail.com>

--- examples/dev/scalar/boxy.jl
+++ examples/dev/scalar/boxy.jl
@@ -1,3 +1,4 @@
+
 #= 
 Proof of concept of 2D localization against a scalar field.
 In this example we use a sequence of 1D measurements and a known map with a
@@ -18,6 +19,7 @@ using Gadfly, Colors
 using Cairo
 using RoMEPlotting
 using Plots
+using JSON2
 # using ImageView
 
 Gadfly.set_default_plot_size(35cm,25cm)
@@ -64,7 +66,6 @@ function driveLeg(x0, terrain, v, units)
 end
 
 
-##
 
 
 # 1. This is the (constant) scalar field the elevation measurements refer to
@@ -157,62 +158,122 @@ function IIF.getSample(s::CalcFactor{<:ScalarFieldSequenceFactorNorthSouth}, N::
 end
 
 
+
+
+function driveLeg!( fg, 
+                    startingPose, 
+                    start,                    
+                    v,
+                    terr,
+                    direction::Symbol)
+  # fg - factor graph object
+  # startingPose
+  # start - 
+  # v - displacement vector (e.g. [NS; 0.0] for direction= :north)
+  # terr - terrain vector (1D interpolant object)
+  # direction - direction symbol (:north,:south,:east,:west)
+
+  # add end pose of the leg
+  newPose = nextPose(startingPose)
+  addVariable!(fg, newPose, Point2, tags=[:POSE,direction])
+
+  addFactor!(fg, [newPose;], PartialPrior(Normal(0.1, 0.1),(2,)))
+
+  p2p = Point2Point2(MvNormal(v, [1.0; 1.0]))
+  addFactor!(fg, [startingPose; newPose], p2p, tags=[:ODOMETRY; direction])
+
+  # generate odometry and measurement data for this leg
+  # avert your eyes: this assumes v is axis-aligned
+  # To pay this technical debt: generate a 2D linspace and use it to query a 2D DEM interpolant
+  x_seq = LinRange(start, start + maximum(v), 100) # assumed 100 measurements per leg
+
+  z_seq = terr.(x_seq)
+  # add noise to x_seq (odometry) and z_seq (meas noise)
+  x_seq_n = 0.01*randn(100)
+  z_seq_n = 1.0*randn(100)
+  
+  someDict = Dict(:x_seq => x_seq, :z_seq => z_seq, :x_seq_n => x_seq_n, :z_seq_n => z_seq_n)
+  addData!(fg, :default_folder_store, newPose, :elevationSequences, Vector{UInt8}(JSON2.write( someDict )), mimeType="application/json/octet-stream"  )
+  
+  return newPose
+end
+
+
+
+# matchLeg!(fg, [:x1, :x5], :NORTH)
+# matchLeg!(fg, [:x3, :x7], :SOUTH)
+function matchLeg!( fg::AbstractDFG,
+                    legs, 
+                    direction::Symbol;
+                    dofactor::Bool=false)
+  # 
+  # ls(fg, tags=[:NORTH])
+
+  dataEntry, dataBytes = getData(fg, legs[1], :elevationSequences)
+  myData_a = JSON2.read(IOBuffer(dataBytes))
+
+  dataEntry, dataBytes = getData(fg, legs[2], :elevationSequences)
+  myData_b = JSON2.read(IOBuffer(dataBytes))
+
+  zseq_a = myData_a[:z_seq]
+  zseq_b = myData_b[:z_seq]
+
+  # correlate z_seq0 against z_seq_m4
+
+  # FIXME ON FIRE dont have odo ground truth
+  # see also: [x,i] = ssdcorr((x,z)_a, (x,z)_b)
+  x_seq = collect( LinRange(0,15,101)[1:100] )
+  intensity = _mySSDCorr(zseq_a, zseq_b)
+
+  bel = AliasingScalarSampler(x_seq, intensity)
+  plr = PartialLinearRelative(bel, (1,))
+
+  f_ = if dofactor
+    addFactor!(fg, legs, plr, tags=[:TERRAIN,:MATCH,direction])
+  end
+
+  return (x_seq, intensity), f_
+end
+
+
 ##
 
 nextPose(ps::Symbol; pattern=r"x") = Symbol("x",match(r"\d+", string(ps)).match |> x->(parse(Int,x)+1))
 
 # drive clockwise, x is North, y is East (NED convention).
 # boxes start bottom left, spine of boxy helix is on x-axis
-function driveOneBox!(fg, 
-                      terrain; 
+function driveOneBox!(fg;
                       lastPose=sortDFG(ls(fg, tags=[:POSE]))[end],
-                      start = 0.0, 
+                      start = [0.0;0.0],
+                      runback = 2/3,
                       NS = 15,
-                      EW = 15  )
+                      EW = 15,
+                      docorr::Bool=false  )
+  #
+  global terrE, terrW
   #
   img = load(joinpath(dirname(dirname(pathof(Caesar))), "examples/dev/scalar/dem.png")) .|> Gray
   h = 1e3*Float64.( @view img[512,:])
-  runback = 2/3
+  start_x, start_y = (start...,)
   
-  # drive North NS units
-  newPose = nextPose(lastPose)
-  addVariable!(fg, newPose, Point2, tags=[:POSE;:NORTHING])
-  # constrain variable in Y (east/west)
-  addFactor!(fg, [newPose;], PartialPrior(Normal(0.0, 0.1),(2,)))
-  # add odometry constraint
-  p2p = Point2Point2(MvNormal([NS; 0.0], [1.0; 1.0]))
-  addFactor!(fg, [lastPose; newPose], p2p, tags=[:ODOMETRY;:NORTHING])
-  lastPose = newPose
+  # drive North NS units (northbound uses west slice)
+  lastPose = driveLeg!(fg, lastPose, start_x, [NS;0.0] ,terrW, :NORTH)
+  start_x = start_x+NS
 
-  # drive East EW units
-  newPose = nextPose(lastPose)
-  addVariable!(fg, newPose, Point2, tags=[:POSE;:EASTING])
-  # constrain variable in Y (east/west)
-  addFactor!(fg, [newPose;], PartialPrior(Normal(EW, 0.1),(2,)))
-  p2p = Point2Point2(MvNormal([0.0, EW], [1.0; 1.0]))
-  addFactor!(fg, [lastPose; newPose], p2p, tags=[:ODOMETRY;:EASTING])
-  lastPose = newPose
+  # drive East EW units (terrain does not matter here)
+  lastPose = driveLeg!(fg, lastPose, start_y ,[0.0; EW], terrW, :EAST)
+  start_y = start_y + EW
   
   # drive South 0.7NS units
-  newPose = nextPose(lastPose)
-  addVariable!(fg, newPose, Point2, tags=[:POSE;:SOUTHING])
-  # constrain variable in Y (east/west)
-  addFactor!(fg, [newPose;], PartialPrior(Normal(EW, 0.1),(2,)))
-  p2p = Point2Point2(MvNormal([-runback*NS; 0.0], [1.0; 1.0]))
-  addFactor!(fg, [lastPose; newPose], p2p, tags=[:ODOMETRY;:SOUTHING])
-  lastPose = newPose
-  
-  # drive West EW units
-  newPose = nextPose(lastPose)
-  addVariable!(fg, newPose, Point2, tags=[:POSE;:WESTING])
-  # constrain variable in Y (east/west)
-  addFactor!(fg, [newPose;], PartialPrior(Normal(0.0, 0.1),(2,)))
-  p2p = Point2Point2(MvNormal([0.0; -EW], [1.0; 1.0]))
-  addFactor!(fg, [lastPose; newPose], p2p, tags=[:ODOMETRY;:WESTING])
-  lastPose = newPose
+  lastPose = driveLeg!(fg, lastPose, start_x, [-runback*NS;0.0],terrE, :SOUTH)
+  start_x = start_x - runback*NS
 
-  # cop out early on first run, or debug
-  dofactor ? nothing : (return nothing)
+  # drive West EW units (terrain does not matter here)
+  lastPose = driveLeg!(fg, lastPose, start_y, [0.0; -EW], terrE, :WEST)
+  start_y = start_y - EW
+
+  # cop-out early on first run, or debug
+  docorr ? nothing : (return nothing)
 
   # Assume adding factors back to PREVIOUS boxy 
   # EAST SIDE
@@ -274,22 +335,77 @@ end
 
 ## build the poses
 
-fg = initfg()
+# trajectory parameters
 NS = 15
+runback = 2/3
 
-addVariable!(fg, :x0, Point2, tags=[:POSE;])
-addFactor!(fg, [:x0;], PriorPoint2(MvNormal([0;0.0], [0.01;0.01])))
+# setup the factor graph
+fg = initfg()
 
-driveOneBox!(fg, nothing, start=0.0, NS=NS)
-
+# adding a blog store
+# getSolverParams(fg).logpath = pwd()
+storeDir = joinLogPath(fg,"data")
+mkpath(storeDir)
+datastore = FolderStore{Vector{UInt8}}(:default_folder_store, storeDir) 
+addBlobStore!(fg, datastore)
 
 
 ##
 
+# actually start adding nodes 
+addVariable!(fg, :x0, Point2, tags=[:POSE;])
+addFactor!(fg, [:x0;], PriorPoint2(MvNormal([0;0.0], [0.01;0.01])))
+
+
+# drive first box, no correlations
+driveOneBox!(fg, runback=runback, start=[0.0;0], NS=NS, docorr=false)
+
 # plr = PartialLinearRelative(bel, (1,))
 # plr = PartialLinearRelative(bel, (1,))
 
-driveOneBox!(fg, nothing, start=NS, NS=NS)
+# drive second box, implement correlation externally
+driveOneBox!(fg, runback=runback, start=[(1-runback)*NS;0], NS=NS, docorr=false)
+
+
+xsq, f_ = matchLeg!(fg, [:x1, :x5],:NORTH, dofactor=false)
+
+xsq, f_ = matchLeg!(fg, [:x3, :x7],:SOUTH, dofactor=false)
+
+## ## TEMPORARY CORRELATION DEV
+
+
+
+
+
+# addFactor!(fg, legs, plr, tags=[:TERRAIN, :MATCH])
+
+##
+
+# get pose four poses back
+# get terrain sequence between (newPose - NW) up to (newPose)
+# curr_seq = terrE(curr_x_seq)
+# prev_seq = terrE(prev_x_seq)
+# # compute SSDcorr wrt sequence 4 poses before
+# # add PartialLinearRelative between the two 
+# intensity = _mySSDCorr(curr_seq, prev_seq)
+
+plr = ScalarFieldSequenceFactorNorthSouth(x_seq_, z_seq, (1,))
+addFactor!(fg, [:x0; :x4], plr, tags=[:TERRAIN,:MATCH])
+    # plr = ScalarFieldSequenceFactorNorthSouth(x_seq, z_seq, (1,))
+    # # # plr = PartialLinearRelative(grid, AliasingScalarSampler(intensity))
+    # addFactor!(fg, [prevPose newPose], plr, tags=[:TERRAIN,:MATCH])
+
+
+
+
+
+
+## ## TEMPORARY
+
+
+##
+
+
 
 
 bel = Normal(10,1.0)
@@ -301,8 +417,6 @@ plr = PartialLinearRelative(bel, (1,))
 
 
 ##
-
-# driveOneBox!(fg, nothing, start=2*NS, NS=NS)
 
 
 
