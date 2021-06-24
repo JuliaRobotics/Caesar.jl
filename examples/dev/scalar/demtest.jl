fb6fc91c066d420ddfcd1bdab81e94214e072344
dehann <dehann.fourie@gmail.com>
2021-06-23 23:17:10 -0400

    next is HeatmapSampler
    Co-authored-by: Pedro Vaz Teixeira <pvazteixeira@gmail.com>

--- examples/dev/scalar/demtest.jl
+++ examples/dev/scalar/demtest.jl
@@ -108,7 +108,7 @@ addBlobStore!(fg, datastore)
 @time loadDEM!(fg, img, (x), (y), meshEdgeSigma=sigma);
 
 # 2. generate trajectory 
-cb(g, lbl) = @show lbl
+cb(g, lbl) = @show lbl # modify to generate elevation measurements (data/smallData as in Boxy) and priors
 @time generateCanonicalFG_Helix2DSlew!(100, posesperturn=30, radius=500, dfg=fg, graphinit=false, slew_y=2/3, postpose_cb=cb)
 
 # check: query variables
@@ -138,8 +138,9 @@ poses = ls(fg, r"x\d+") |> sortDFG
 
 for p in poses
     e = elevation(p) 
-    # add to variable
 
+    # add to variable
+    
 end
 
 # 4a. add pairwise constraints between poses and points
@@ -148,3 +149,178 @@ end
 
 
 
+
+## ## DEV TESTING BELOW
+
+using ImageView
+using UnicodePlots
+
+imshow(img)
+
+## Aliasing sampler on sparse selection of points
+
+@info "DEM value range" minimum(img) maximum(img)
+
+
+
+function getLevelSet(data,x, y, level, tol=0.01)
+    """
+    Get the grid positions at the specified height
+    """
+    roi = data.-level
+    mask = abs.(roi) .< tol
+
+    idx2d = findall(mask)  # 2D indices
+    # idx   = i:length(idx)
+    (v->[x[v.I[1]],y[v.I[2]]]).(idx2d)
+end
+
+# go bigger than measurement noise, to also accomodate heatmap sampling process errors
+R_meas = 1e-4
+
+# selection of interest, assume elevation measurement aroun d0.5 is of interest
+level = 0.5
+img_soi = img .- level
+immask = abs.(img_soi) .< 0.01
+
+imshow(immask)
+
+img_sparse = img_soi[immask]
+idx = findall(immask)
+
+idx_pos = (v->[x[v[1]],y[v[2]]]).(idx)
+
+x_spacing = x[2]-x[1]
+y_spacing = y[2]-y[1]
+
+# imgT = zeros(100,100)
+# imgT[10,:] .= 1.0
+
+# imshow(imgT)
+
+# OPTION A
+kerBw_ = 0.7*0.5*(x_spacing + y_spacing) # 70%
+kerBw = [kerBw_; kerBw_]
+@cast kerPos_[i,j] := idx_pos[j][i]
+kerPos = collect(kerPos_)
+P_pre = kde!(kerPos, kerBw)
+pts_preIS = sample(P_pre, 10000)[1]
+
+# draw flipped so consistent with imshow img[i,j] == img[x,y]
+imscatter(img_) = scatterplot(img_[2,:], -img_[1,:])
+imscatter(pts_preIS)
+
+#
+densityplot(pts_preIS[2,:], -pts_preIS[1,:])
+
+# weigth the samples according to interpolated heatmap
+
+
+
+# still need heatmap interpolation eval for weighting
+# weight = heatmap(pts_preIS)
+# R_meas helps control sample process sensitivity
+hm = Interpolations.LinearInterpolation((x,y), img .- level ) # interpolated DEM
+
+@cast vec_preIS[j][i] := pts_preIS[i,j]
+
+delev = Vector{Float64}(undef, length(vec_preIS))
+
+function hack(w_, i, u)
+    w_[i] = hm(u...)
+    nothing
+end
+
+##
+
+using ProgressMeter
+
+##
+
+@showprogress 1 "wait what...?" for (i,u) in enumerate(vec_preIS)
+  @show i, u
+  if 9000 < abs(u[1]) || 9000 < abs(u[2]) 
+    delev[i] = 0.0
+    continue
+  end
+  hack(delev, i, u)
+end
+
+##
+
+weights = exp.(-(0.5/R_meas)*(abs.(delev)).^2)
+weights ./= sum(weights)
+
+histogram(weights, nbins=20, closed=:left)
+
+# get final kde
+
+# bw = [0.1;0.1]
+prior = kde!(pts_preIS, kerBw)
+
+
+
+##
+
+
+# # OPTION B
+# # where are the values of interest
+# # make a distribution
+# img_sparse .^= 2
+# img_pdf = exp.(-img_sparse*0.5/R_meas)
+# img_pdf ./= sum(img_pdf)
+# bss = AliasingScalarSampler([id_idx;]./1.0, img_pdf)
+# id_smpls = rand(bss, 10000) .|> Int
+
+
+
+# ## how to super sample massive low res grid 2D scalar field
+
+# # binary threshold the gridded heatmap
+
+# # OPTION A
+# # P_pre is weighted KDE kernels with fat bandwidth at each grid point (scalar dictates weight of kernel)
+# # pts_preIS = sample(P_pre, 10000)
+# # weight = heatmap(pts_preIS)
+
+# # OPTION B
+# # get first "grid points of interest" from heatmap with aliasing sampler 
+# # mush those grid points to loose clusters (conv with Guassian)
+
+# # importance weight those loose cluster points against the heatmap-interpolated
+# N = 1000
+# ## how to build weighted KDE (not full DEM pipeline yet)
+# # mock heatmap
+# mock = MvNormal([0.0;0.0], [1.0 0.5; 0.5 1.0])
+# pts = rand(Mv, N)
+# # mock mask
+# mask = pts[2,:] .< 0.5
+
+# # importance weighted samples from "heatmap
+# weights = ones(N)
+# weights[mask] .= 0.01
+# weights ./= sum(weights)
+# postMushWeightedPts = pts
+
+# bw = [0.1;0.1]
+# # this is the KDE belief to build a prior for localizaton from low res map (final step)
+# P = kde!(postMushWeightedPts, bw, weights)
+
+
+
+
+
+
+##
+
+
+
+
+##
+
+using KernelDensityEstimatePlotting
+Gadfly.set_default_plot_size(35cm,25cm)
+
+plotKDE(prior)
+
+#
