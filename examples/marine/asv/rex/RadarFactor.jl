22ae1336704f53071479a271beb112f4c3af03b1
dehann <dehann.fourie@gmail.com>
2021-10-12 14:09:19 -0400

    update radar factor

--- examples/marine/asv/rex/RadarFactor.jl
+++ examples/marine/asv/rex/RadarFactor.jl
@@ -16,6 +16,7 @@ This is but one incarnation for how radar alignment factor could work, treat it 
 Notes
 - Stanard `cvt` argument is lambda function to convert incoming images to user convention of image axes,
   - default `cvt` flips image rows so that Pose2 xy-axes corresponds to img[x,y] -- i.e. rows down and across from top left corner.
+- Use rescale to resize the incoming images for lower resolution (faster) correlations
 
 Example
 -------
@@ -27,11 +28,15 @@ arp2 = AlignRadarPose2(sweep[10], sweep[11], MvNormal(zeros(3), diagm([5;5;pi/4]
 struct AlignRadarPose2{T} <: IIF.AbstractManifoldMinimize
   im1::Matrix{T}
   im2::Matrix{T}
+  gridlength::Float64
+  rescale::Float64
 
   # replace inner constructor with transform on image
   AlignRadarPose2{T}( im1::AbstractMatrix{T}, 
                       im2::AbstractMatrix{T},
-                      cvt = (im)->reverse(im, dims=1) # convert incoming image
+                      gridlength::Real,
+                      rescale::Real=1,
+                      cvt = (im)->reverse(imresize(im,trunc.(Int, rescale.*size(im))),dims=1)
                     ) where {T} = new{T}( cvt(im1), 
                                           cvt(im2) )
 end
@@ -63,7 +68,7 @@ function (cf::CalcFactor{<:AlignRadarPose2})(X, p, q)
   # e0 = identity_element(M, p)
   rp2 = cf.factor
   
-  tf = Manifolds.compose(M, inv(p), q) # for groups
+  tf = Manifolds.compose(M, inv(M, p), q) # for groups
   
   return evaluateTransform(rp2.im1, rp2.im2, tf)
   
@@ -77,6 +82,9 @@ end
 struct PackedAlignRadarPose2 <: PackedInferenceType
   im1::Vector{Vector{Float64}}
   im2::Vector{Vector{Float64}}
+  gridlength::Float64
+  rescale::Float64
+
   # PreSampler::String
   # p2p2::PackedPose2Pose2
 end
@@ -88,9 +96,9 @@ function convert(::Type{<:PackedAlignRadarPose2}, arp2::AlignRadarPose2)
   TensorCast.@cast pim2[row] := collect(pim2[row])
   PackedAlignRadarPose2(
     pim1,
-    pim2 )
-    # convert(PackedSamplableBelief, arp2.PreSampler),
-    # convert(PackedPose2Pose2, arp2.p2p2))
+    pim2,
+    arp2.gridlength,
+    arp.rescale )
 end
 
 function convert(::Type{<:AlignRadarPose2}, parp2::PackedAlignRadarPose2)
@@ -98,7 +106,7 @@ function convert(::Type{<:AlignRadarPose2}, parp2::PackedAlignRadarPose2)
   TensorCast.@cast im2[row,col] := parp2.im2[row][col]
   AlignRadarPose2(
     collect(im1),
-    collect(im2) )
-    # convert(SamplableBelief, parp2.PreSampler), #   extractdistribution(parp2.PreSampler),
-    # convert(Pose2Pose2, parp2.p2p2))
+    collect(im2),
+    parp2.gridlength,
+    parp2.rescale )
 end
