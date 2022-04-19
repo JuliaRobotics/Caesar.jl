f66ebc1e0f41fe601953f2b4ab2ea35076162158
dehann <dehann.fourie@gmail.com>
2022-04-18 22:54:42 -0700

    wip SAP with stash new test

--- src/images/ScatterAlignPose2.jl
+++ src/images/ScatterAlignPose2.jl
@@ -63,12 +63,20 @@ ScatterAlignPose2(im1::AbstractMatrix{T},
                   bw::Real=5e-5, # from a sensitivity analysis with marine radar data (50 or 100 samples)
                   rescale::Real=1,
                   N::Integer=1000,
-                  cvt = (im)->reverse(Images.imresize(im,trunc.(Int, rescale.*size(im))),dims=1)
+                  cvt = (im)->reverse(Images.imresize(im,trunc.(Int, rescale.*size(im))),dims=1),
+                  useStashing::Bool=false,
+                  dataEntry_cloud1="",
+                  dataEntry_cloud2="",
+                  dataStoreHint=""
                 ) where {T} = ScatterAlignPose2(;cloud1=HeatmapGridDensity(cvt(im1),domain,N=N), 
                                                 cloud2=HeatmapGridDensity(cvt(im2),domain,N=N),
                                                 gridscale=float(rescale),
                                                 sample_count, 
-                                                bw  )
+                                                bw,
+                                                useStashing,
+                                                dataEntry_cloud1 = string(dataEntry_cloud1), 
+                                                dataEntry_cloud2 = string(dataEntry_cloud2),
+                                                dataStoreHint = string(dataStoreHint)  )
 #
 
 getManifold(::IIF.InstanceType{<:ScatterAlignPose2}) = getManifold(Pose2Pose2)
@@ -79,12 +87,15 @@ function preambleCache(dfg::AbstractDFG, vars::AbstractVector{<:DFGVariable}, fn
   M = getManifold(Pose2)
   e0 = ProductRepr(SVector(0.0,0.0), SMatrix{2,2}(1.0, 0.0, 0.0, 1.0))
 
-  # constitute cloud belief from dataEntry
-  if fnc.useStashing && 0 < length(fnc.dataEntry_cloud1)
-    de1, db1 = getData(dfg, getLabel(vars[1]), fnc.dataEntry_cloud1) # fnc.dataStoreHint
-    cld1 = unpackDistribution(db1)
-    update!(fnc.cloud1, cld1)
-
+  # reconstitute cloud belief from dataEntry
+  for (va,de,cl) in zip(vars,[fnc.dataEntry_cloud1,fnc.dataEntry_cloud2],[fnc.cloud1,fnc.cloud2])
+    if fnc.useStashing 
+      @assert 0 < length(de) "cannot reconstitute ScatterAlignPose2 without necessary data entry, only have $de"
+      _, db = getData(dfg, getLabel(va), Symbol(de)) # fnc.dataStoreHint
+      cld = convert(SamplableBelief, String(take!(IOBuffer(db))))
+      # cld = unpackDistribution(strdstr)
+      IIF._update!(cl, cld)
+    end
   end
 
   smps1, = sample(fnc.cloud1, fnc.sample_count)
@@ -265,10 +276,51 @@ Base.@kwdef struct PackedScatterAlignPose2 <: AbstractPackedFactor
   dataStoreHint::String = ""
 end
 
+function parchDistribution(mkd::ManifoldKernelDensity)
+  pts = getPoints(mkd)
+  bw = getBW(mkd)[:,1]
+  manikde!(mkd.manifold, pts[1:1], mkd._u0; bw, partial=mkd._partial, infoPerCoord=mkd.infoPerCoord)
+end
+
+function parchDistribution(hgd::HeatmapGridDensity)
+  @assert 2 <= size(hgd.data,1) "parchDistribution of HeatmapGridDensity can only be done when `.data` is larger than 2x1"
+  
+  data = Matrix{eltype(hgd.data)}(undef, 2,2)
+  data[1,1] = hgd.data[1,1]
+  # data[2,2] = hgd.data[2,2] # disable since data might be a single column in unusual cases
+  data[2,1] = size(hgd.data,1)
+  data[1,2] = size(hgd.data,2)
+
+  domain = hgd.domain
+  hint_callback = hgd.hint_callback
+  bw_factor = hgd.bw_factor
+  densityFnc = parchDistribution(hgd.densityFnc)
+  
+  HeatmapGridDensity(
+    data,
+    domain,
+    hint_callback,
+    bw_factor,
+    densityFnc
+  )
+end
+
+
 function convert(::Type{<:PackedScatterAlignPose2}, arp::ScatterAlignPose2)
 
-  cloud1 = packDistribution(arp.cloud1)
-  cloud2 = packDistribution(arp.cloud2)
+  cld1 = arp.cloud1
+  cld2 = arp.cloud2
+
+  # reconstitute full type during the preambleCache step
+  if arp.useStashing
+    @assert length(arp.dataEntry_cloud1) !== 0 "packing of ScatterAlignPose2 asked to be `useStashing=true`` yet no `.dataEntry_cloud1` exists for later loading"
+    cld1 = parchDistribution(arp.cloud1)
+    @assert length(arp.dataEntry_cloud2) !== 0 "packing of ScatterAlignPose2 asked to be `useStashing=true`` yet no `.dataEntry_cloud2` exists for later loading"
+    cld2 = parchDistribution(arp.cloud2)
+  end
+
+  cloud1 = packDistribution(cld1)
+  cloud2 = packDistribution(cld2)
 
   PackedScatterAlignPose2(;
     cloud1,
@@ -283,7 +335,7 @@ function convert(::Type{<:PackedScatterAlignPose2}, arp::ScatterAlignPose2)
 end
 
 function convert(::Type{<:ScatterAlignPose2}, parp::PackedScatterAlignPose2)
-
+  
   cloud1 = unpackDistribution(parp.cloud1)
   cloud2 = unpackDistribution(parp.cloud2)
   
@@ -297,7 +349,8 @@ function convert(::Type{<:ScatterAlignPose2}, parp::PackedScatterAlignPose2)
     useStashing = parp.useStashing,
     dataEntry_cloud1 = parp.dataEntry_cloud1,
     dataEntry_cloud2 = parp.dataEntry_cloud2,
-    dataStoreHint = parp.dataStoreHint )
+    dataStoreHint = parp.dataStoreHint 
+  )
 end
 
 #
---
+++ test/testStashing_SAP.jl
@@ -0,0 +1,121 @@
+# PoC on stashing for ScatterAlignPose2
+
+using Test
+using Caesar
+using JSON, JSON2
+using Images
+using Manifolds
+import Rotations as _Rot
+
+
+## Build a factor graph with ScatterAlignPose2 factors and set for stashing serialization
+
+
+x = -10:0.1:10;
+y = -10:0.1:10;
+
+σ = 0.1
+
+Σ = Diagonal([σ;σ])
+g = (x,y)->pdf(MvNormal([3.;0],Σ),[x;y]) + pdf(MvNormal([8.;0.0],4*Σ),[x;y]) + pdf(MvNormal([0;5.0],Σ),[x;y])
+
+bIM1 = zeros(length(x),length(y))
+bIM2 = zeros(length(x),length(y))
+
+oT = [2.; 0]
+oΨ =  pi/6
+
+M = SpecialEuclidean(2)
+e0 = identity_element(M)
+pCq = [oT;oΨ]
+qGp = inv(M, exp(M, e0, hat(M, e0, pCq)))
+qTp = affine_matrix(M, qGp )
+
+qCp = vee(M, e0, log(M, e0, qGp))
+
+
+for (i,x_) in enumerate(x), (j,y_) in enumerate(y)
+  bIM1[i,j] = g(x_,y_)
+  v = qTp*[x_;y_;1.0]
+  _x_, _y_ = v[1], v[2]
+  bIM2[i,j] = g(_x_, _y_)
+end
+
+
+##
+
+fg = initfg()
+getSolverParams(fg).inflateCycles=1
+
+##
+
+# getSolverParams(fg).logpath = pwd()
+storeDir = joinLogPath(fg,"data")
+mkpath(storeDir)
+# requires IIF v0.15, DFG v0.10
+datastore = FolderStore{Vector{UInt8}}(:test_stashing_store, storeDir) 
+addBlobStore!(fg, datastore)
+
+##
+
+addVariable!(fg, :x0, Pose2)
+addVariable!(fg, :x1, Pose2)
+
+## must store the stashed data entry blobs before the preambleCache function runs on addFactor
+
+##
+
+sap = ScatterAlignPose2(bIM1, bIM2, (x,y); 
+                        sample_count=100, bw=1.0, 
+                        cvt=(im)->im, 
+                        useStashing=true, 
+                        dataEntry_cloud1=string(:hgd_stash_x0),
+                        dataEntry_cloud2=string(:hgd_stash_x1),
+                        dataStoreHint = string(:test_stashing_store) )
+#
+
+##
+
+db = Vector{UInt8}( convert(String, sap.cloud1) )
+addData!(fg, :test_stashing_store, :x0, :hgd_stash_x0, db, mimeType="application/json/octet-stream")
+
+db = Vector{UInt8}( convert(String,sap.cloud2) )
+addData!(fg, :test_stashing_store, :x1, :hgd_stash_x1, db, mimeType="application/json/octet-stream")
+
+
+##
+
+de, db_ = getData(fg, :x0, :hgd_stash_x0)
+
+# myData = JSON2.read(IOBuffer(db), PackedHeatmapGridDensity)
+
+# JSON.parse(String(take!(IOBuffer(db))))["_type"] |> DFG.getTypeFromSerializationModule
+
+# convert(SamplableBelief, String(take!(IOBuffer(db))))
+
+
+##
+
+addFactor!(fg, [:x0;], PriorPose2(MvNormal([0;0;0.],[0.01;0.01;0.01])))
+f = addFactor!(fg, [:x0;:x1], sap, inflation=0.0)
+
+
+## Test packFactor with useStashing
+
+
+pf = DFG.packFactor(fg, f)
+
+
+## save the factor graph
+
+filepath = joinLogPath(fg, "selftest")
+saveDFG(filepath, fg)
+
+## load the factor graph using unstash 
+
+
+fg_ = loadDFG(filepath)
+
+
+## confirm that the right factor graph has be reconstituted
+
