c2be1b777df2ed3c9d2a41f69f9fcd61081a606b
dehann <dehann.fourie@gmail.com>
2022-08-10 09:42:54 -0700

    better standardize ICP with MJL

--- src/3rdParty/_PCL/services/ConsolidateRigidTransform.jl
+++ src/3rdParty/_PCL/services/ConsolidateRigidTransform.jl
@@ -47,22 +47,27 @@ end
 ## ============================================================
 
 const _SO3_MANI = SpecialOrthogonal(3)
+const _SE3_MANI = SpecialEuclidean(3)
 
 # name euler here is very ambiguous, these are Lie algebra elements
 # used to manipulate cartesian coordinates in a TranslationGroup(3) space.
 function euler_angles_to_linearized_rotation_matrix(α1, α2, α3, rigid::Bool=true)
-  if rigid
+  dR = if rigid
+    # TODO likely faster performance by using a retraction instead of expmap
     exp_lie(_SO3_MANI, hat(_SO3_MANI, Identity(_SO3_MANI), SA[α1, α2, α3]))
   else
-    dR = [  1 -α3  α2
-          α3   1 -α1
-          -α2  α1   1]
+    SMatrix{3,3,Float64}(1.0,0,0,0,1,0,0,0,1) + 
+      hat(_SO3_MANI, Identity(_SO3_MANI), SA[α1, α2, α3])
+    # [ 1 -α3  α2
+    #   α3   1 -α1
+    #  -α2  α1   1]
   end
 end
 
 function create_homogeneous_transformation_matrix(R, t)
-  H = [R          t
-       zeros(1,3) 1]
+  H = affine_matrix(_SE3_MANI, ArrayPartition(t, R))
+  # H = [R          t
+  #      zeros(1,3) 1]
 end
 function euler_coord_to_homogeneous_coord(XE)
   no_points = size(XE, 1)
