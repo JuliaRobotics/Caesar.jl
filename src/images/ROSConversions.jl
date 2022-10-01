ed7842cbfeb1d7593828b7b507fb7301c796023b
dehann <dehann.fourie@gmail.com>
2022-10-01 08:14:32 -0700

    do better on ros conversions

--- src/images/ROSConversions.jl
+++ src/images/ROSConversions.jl
@@ -1,27 +1,11 @@
 @info "Caesar.jl is loading tools relating to Images.jl and RobotOS.jl"
 
+# not sure which of these are necessary with ros/CaesarROS.jl
 using .RobotOS
-
 import Unmarshal: unmarshal
-
-@rosimport std_msgs.msg: Header
-@rosimport sensor_msgs.msg: Image
+# @rosimport sensor_msgs.msg: Image
 
 
-function unmarshal(
-  header::Main.std_msgs.msg.Header
-)
-  #
-  Dict{String,Any}( 
-    "seq" => header.seq,
-    "stamp" => Dict{String,Int}(
-      "secs"=>header.stamp.secs, 
-      "nsecs"=>header.stamp.nsecs
-    ),
-    "frame_id" => header.frame_id,
-    "_type" => "ROS1/std_msgs/Header" 
-  )
-end
 
 function unmarshal(
   msg::Main.sensor_msgs.msg.Image
