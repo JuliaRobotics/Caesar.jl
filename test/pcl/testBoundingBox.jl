4f8daee474c20769647f84249b04d23c2c869fdb
dehann <dehann.fourie@gmail.com>
2022-12-26 19:06:29 -0800

    fix bounding box test

--- test/pcl/testBoundingBox.jl
+++ test/pcl/testBoundingBox.jl
@@ -11,13 +11,14 @@ import Caesar._PCL as _PCL
 
 p0 = [0,0,0.] # GeoB.Point(
 p1 = [0,0,1.] # GeoB.Point(
+p1b = [0.1,0.1,1.1] # GeoB.Point(
 p2 = [1,1,2.] # GeoB.Point(
 p3 = [0,0,4.] # GeoB.Point(
 p4 = [0.5;0.5;2]
 
 hr = _PCL.AxisAlignedBoundingBox( p1,p2 )
 
-@test _PCL.inside(hr, p1)
+@test _PCL.inside(hr, p1b)
 @test !_PCL.inside(hr, p0)
 @test !_PCL.inside(hr, p3)
 @test _PCL.inside(hr, p4)
